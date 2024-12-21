557a268583fa73380012b2d7135a818bda680e45
Christian Knapp <schmolldechse@icloud.com>
2024-12-21 18:18:08 +0100

    added fetching of departures

--- src/app/[id]/departures/page.tsx
+++ src/app/[id]/departures/page.tsx
@@ -1,16 +1,57 @@
 'use client';
 
 import {useParams} from "next/navigation";
+import {useEffect, useState} from "react";
+import {Scheduled} from "@/app/lib/schedule";
 
 export default function Departures() {
     const params = useParams();
     const id = params.id;
 
-    console.log(id);
+    const [scheduled, setScheduled] = useState<Scheduled[]>([]);
+
+    // fetch departures from HAFAS
+    useEffect(() => {
+        const currentUnix = new Date().toISOString();
+
+        const fetchDepartures = async () => {
+            const response = await fetch(`https://hafas.voldechse.wtf/stops/${id}/departures?when=${currentUnix}&duration=60&results=1000`, {method: 'GET'});
+            if (!response.ok) return;
+
+            const data = await response.json();
+            if (!data?.departures) return;
+            if (!Array.isArray(data.departures)) return;
+
+            const filtered: Scheduled[] = data.departures
+                .filter((departure: any) => departure.tripId)
+                .map((departure: any) => ({
+                    tripId: departure.tripId,
+                    plannedWhen: departure.plannedWhen,
+                    actualWhen: departure.when,
+                    delay: departure.delay,
+                    plannedPlatform: departure.plannedPlatform,
+                    actualPlatform: departure.platform,
+                    directionName: departure.destination.name,
+                    directionId: departure.destination.id,
+                    line: departure.line
+                }));
+            setScheduled(filtered);
+        }
+
+        fetchDepartures();
+    }, []);
 
     return (
         <>
-            <p className={"text-white"}>Hallo!</p>
+            {scheduled.length > 0 ? (
+                scheduled.map((scheduled: Scheduled) => (
+                    <p key={scheduled.tripId}
+                       className="text-white py-0.5 px-2 hover:bg-gray-700 hover:cursor-pointer rounded-md"
+                    >
+                        {scheduled.line.name}
+                    </p>
+                ))
+            ) : ( <></> )}
         </>
     )
 }
---
+++ src/app/lib/schedule.ts
@@ -0,0 +1,17 @@
+export type Scheduled = {
+    tripId: string,
+    plannedWhen: string,
+    actualWhen: string,
+    delay: number, // in sec
+    plannedPlatform: string,
+    actualPlatform: string,
+    directionName: string, // name of the destination
+    directionId: string, // id of the destination
+    line: Line
+}
+
+type Line = {
+    id: string,
+    name: string,
+    operator: string
+}
