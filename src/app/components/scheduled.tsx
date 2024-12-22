ca06ec0134ce1e32d1961755070a02bd66f17be3
Christian Knapp <schmolldechse@icloud.com>
2024-12-22 17:23:10 +0100

    displaying platform

--- src/app/components/scheduled.tsx
+++ src/app/components/scheduled.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import {ScheduledLine} from "@/app/lib/schedule";
 
 interface ScheduledProps {
@@ -17,7 +15,7 @@ const ScheduledComponent: React.FC<ScheduledProps> = ({scheduled, isEven}) => {
     }
 
     return (
-        <div className="container mx-auto flex justify-between space-x-4 text-white text-base font-bold border-gray-400 pb-4"
+        <div className="container mx-auto flex justify-between space-x-4 text-white text-2xl font-bold border-gray-400 pb-4"
              style={{backgroundColor}}
         >
             {/* First col */}
@@ -30,8 +28,8 @@ const ScheduledComponent: React.FC<ScheduledProps> = ({scheduled, isEven}) => {
                 </span>
 
                 {/* Departure time */}
-                <div className="flex flex-row items-center justify-end space-x-4 text-xl">
-                    <span className={`${isDelayed() ? 'line-through text-red-500' : ''}`}>
+                <div className="flex flex-row items-center justify-end space-x-4">
+                    <span className={`${isDelayed() ? 'line-through text-red-500 text-xl' : ''}`}>
                         {new Date(scheduled.plannedWhen).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}
                     </span>
                     {isDelayed() ? (
@@ -43,14 +41,18 @@ const ScheduledComponent: React.FC<ScheduledProps> = ({scheduled, isEven}) => {
             </div>
 
             {/* Second col */}
-            <div className="flex-[4] text-left border-t pt-4">
+            <div className="flex-[4] text-left border-t pt-4 px-2">
                 <p>Über</p>
                 <p>Ziel</p>
             </div>
 
             {/* Third col */}
-            <div className="flex-[1] flex justify-end items-end text-right border-t pt-4">
-                <p>Gleis</p>
+            <div className="flex-[1] flex justify-end items-end text-right border-t pt-4 px-2">
+                {scheduled.plannedPlatform === scheduled.actualPlatform ? (
+                    <span>{scheduled.plannedPlatform}</span>
+                ) : (
+                    <span className="bg-red-500 w-full px-6 py-2">{scheduled.actualPlatform}</span>
+                )}
             </div>
         </div>
     );
