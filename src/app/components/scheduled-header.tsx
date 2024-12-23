172b43cbec06e8ef2a3dd660f6112de63d0f0297
Christian Knapp <schmolldechse@icloud.com>
2024-12-23 19:19:58 +0100

    fixed: incorrect padding

--- src/app/components/scheduled-header.tsx
+++ src/app/components/scheduled-header.tsx
@@ -1,6 +1,6 @@
 const ScheduledHeader: React.FC = () => {
     return (
-        <div className="container mx-auto flex justify-between space-x-4 text-white text-base p-2">
+        <div className="container mx-auto flex justify-between space-x-4 text-white text-base">
             {/* First col */}
             <div className="flex-[1] text-right mr-8">
                 <p>Zug / Bus</p>
