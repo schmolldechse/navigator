08926739b789f3d48e8c8fe59c9bf62b2ba1d47f
Christian Knapp <schmolldechse@icloud.com>
2024-12-26 22:07:48 +0100

    remove unnecessary status code

--- src/app/api/v1/color/route.ts
+++ src/app/api/v1/color/route.ts
@@ -5,10 +5,10 @@ export async function GET(req: NextRequest) {
     const { searchParams } = new URL(req.url);
     const id = searchParams.get("id");
 
-    if (!id) return NextResponse.json({ success: false, error: 'Id is required' }, { status: 400 });
+    if (!id) return NextResponse.json({ error: 'Id is required' }, { status: 400 });
 
     const lineData = await fromHafasLineId(id);
-    if (!lineData) return NextResponse.json({ success: false, error: 'Line not found' }, { status: 404 });
+    if (!lineData) return NextResponse.json({ error: 'Line not found' });
 
     return NextResponse.json(lineData);
 }
