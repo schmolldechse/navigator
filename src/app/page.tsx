90a330994785e23b0b3dd158c51580837d8763e5
Christian Knapp <schmolldechse@icloud.com>
2025-01-03 13:25:24 +0100

    removed annoying tabIndex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
     }
 
     return (
-        <div className="flex justify-center mt-[12.5%]" onKeyDown={handleKeyInput} tabIndex={0}>
+        <div className="flex justify-center mt-[12.5%]" onKeyDown={handleKeyInput}>
             <div className="flex-col bg-[#000] p-4 rounded-xl w-[90%] max-w-[600px]">
                 <div className="flex space-x-4">
                     <Image width={40} height={40} src={"/search.svg"} alt={"Search icon"}/>
