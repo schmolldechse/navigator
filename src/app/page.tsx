e3ae4d6f811c0a34e5dc68e62189d96dde044444
Christian Knapp <schmolldechse@icloud.com>
2025-01-10 23:03:23 +0100

    minor changes
    - switched to new station api
    - added delay before starting the request
    
    fetch stations from DB vendo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,61 @@
 'use client';
 
-import {useState} from "react";
+import {useRef, useState} from "react";
 import Image from "next/image";
 import {useRouter} from "next/navigation";
-
-type LocationResult = {
-    id: number,
-    name: string
-}
+import {Station} from "@/app/lib/objects";
 
 export default function Home() {
     const router = useRouter();
-    const [results, setResults] = useState<LocationResult[]>([]);
+    const [stations, setStations] = useState<Station[]>([]);
 
     const [focusedIndex, setFocusedIndex] = useState<number | undefined>(null);
 
-    const handleSearch = async (query: string) => {
-        const response = await fetch(`https://vendo-prof-db.voldechse.wtf/locations/?query=${query}&addresses=false&results=5`, {method: 'GET'});
+    const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+    const handleInput = (value: string) => {
+        if (value.trim().length === 0) {
+            setStations([]);
+            return;
+        }
+
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
+        timeoutRef.current = setTimeout(() => searchStations(value), 500);
+    }
+
+    const searchStations = async (query: string) => {
+        const response = await fetch(`/api/v1/station`, {
+            method: 'POST',
+            body: JSON.stringify({query: query})
+        });
         if (!response.ok) return;
 
         const data = await response.json();
+        if (!data.entries || data.entries.length === 0) return;
 
-        const filtered: LocationResult[] = data
-            .filter((location: any) => location.id && location.name)
-            .map((location: any) => ({
-                id: location.id,
-                name: location.name
-            }));
-        setResults(filtered);
+        const filtered: Station[] = data.entries
+            .filter((location: any) => location.evaNr && location.name)
+            .map((location: any) => location as Station);
+        setStations(filtered);
         setFocusedIndex(null);
     }
 
     const handleKeyInput = (e: React.KeyboardEvent<HTMLDivElement>) => {
-        if (results.length === 0) return;
+        if (stations.length === 0) return;
 
         switch (e.key) {
             case 'ArrowDown':
             case 'Tab':
                 e.preventDefault();
-                setFocusedIndex((prev) => (prev === null || prev === results.length - 1 ? 0 : prev + 1));
+                setFocusedIndex((prev) => (prev === null || prev === stations.length - 1 ? 0 : prev + 1));
                 break;
             case 'ArrowUp':
                 e.preventDefault();
-                setFocusedIndex((prev) => (prev === null || prev === 0 ? results.length - 1 : prev - 1));
+                setFocusedIndex((prev) => (prev === null || prev === 0 ? stations.length - 1 : prev - 1));
                 break;
             case 'Enter':
                 if (focusedIndex === null) return;
-                router.push(`/${results[focusedIndex].id}/departures`);
+                router.push(`/${stations[focusedIndex].evaNr}/departures`);
                 break;
             default:
                 break;
@@ -59,25 +68,17 @@ export default function Home() {
                 <div className="flex space-x-4">
                     <Image width={40} height={40} src={"/search.svg"} alt={"Search icon"}/>
                     <input type="text" placeholder={"Search your station"}
-                           onChange={(e) => {
-                               const value: string = e.target.value;
-                               if (value.trim().length === 0) {
-                                   setResults([]);
-                                   return;
-                               }
-
-                               handleSearch(value);
-                           }}
+                           onChange={(e) => handleInput(e.target.value)}
                            className={"bg-[#000] text-xl p-3 w-full border rounded-lg border-slate-500 placeholder-slate-400 focus:outline-none"}
                     />
                 </div>
 
-                <div className={`flex-row ${results.length > 0 ? 'mt-4' : ''}`}>
-                    {results.length > 0 && results.map((location: LocationResult, index) => (
+                <div className={`flex-row ${stations.length > 0 ? 'mt-4' : ''}`}>
+                    {stations.length > 0 && stations.map((location: Station, index) => (
                         <p
-                            key={location.id}
+                            key={location.evaNr}
                             className={`text-white py-0.5 px-2 hover:bg-gray-700 hover:cursor-pointer rounded-md ${focusedIndex === index ? 'bg-gray-700' : ''}`}
-                            onClick={() => router.push(`/${location.id}/departures`)}
+                            onClick={() => router.push(`/${location.evaNr}/departures`)}
                             tabIndex={0}
                             role="button"
                             aria-pressed={focusedIndex === index}
