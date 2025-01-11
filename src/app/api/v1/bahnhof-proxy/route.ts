05f96a0765ae56f06c7b683f0bd1896b988fb795
Christian Knapp <christian.alex.knapp@icloud.com>
2025-01-11 22:25:08 +0100

    fixed missing import

--- src/app/api/v1/bahnhof-proxy/route.ts
+++ src/app/api/v1/bahnhof-proxy/route.ts
@@ -1,5 +1,6 @@
 import {NextRequest, NextResponse} from "next/server.js";
 import {Connection, Journey} from "@/app/lib/objects";
+import {mapStops} from "@/app/lib/utils";
 
 export async function GET(req: NextRequest) {
     const {searchParams} = new URL(req.url);
