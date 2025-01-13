3b893631f262b0b86a8d079519086cccb7e790cc
Christian Knapp <christian.alex.knapp@icloud.com>
2025-01-13 15:05:56 +0100

    simplified spacing, removed text-color

--- src/app/components/scheduled-header.tsx
+++ src/app/components/scheduled-header.tsx
@@ -5,19 +5,19 @@ interface HeaderProps {
 const ScheduledHeader: React.FC<HeaderProps> = ({isDeparture}) => {
     return (<>
         {/* layout for smaller screens (under md) */}
-        <div className="contaienr mx-4 flex justify-between text-white text-sm md:hidden">
+        <div className="container mx-4 flex justify-between text-sm md:hidden">
             <p className="text-left">Zug / Bus</p>
             <p className="text-right">Gleis</p>
         </div>
 
         {/* layout for greater screens (above md) */}
-        <div className="container mx-auto hidden md:flex justify-between space-x-4 text-white text-base">
+        <div className="container mx-auto hidden md:flex justify-between text-base">
             <div className="flex-[1] text-right mr-8">
                 <p>Zug / Bus</p>
                 <p>Zeit</p>
             </div>
 
-            <div className="flex-[4] text-left">
+            <div className="flex-[4] text-left mr-4">
                 <p>Über</p>
                 <p>{isDeparture ? 'Ziel' : 'Herkunft'}</p>
             </div>
