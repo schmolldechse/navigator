dcaf7890ad6cb125dce1cba9982fd0624bf35d90
Christian <150681026+schmolldechse@users.noreply.github.com>
2025-01-15 18:16:24 +0100

    fixes issues (#31)
    * fixes #29: remove direction-based matching for connections
    
    This change should not have a negative effect on other mappings. If no connections is found to map, the unmatched connection will simply be skipped.
    
    * fixes #30: dynamically adjust SVG width based on visible circles

--- src/app/components/show-more.tsx
+++ src/app/components/show-more.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface Props {
     onClick: () => void;
 }
@@ -5,27 +7,21 @@ interface Props {
 const ShowMore: React.FC<Props> = ({onClick}) => {
     return (
         <span className="ml-2 inline-flex items-center justify-center cursor-pointer px-2 py-1 rounded-2xl bg-gray-700 group"
-              onClick={onClick} style={{verticalAlign: 'middle'}}>
-            <svg
-                width="22"
-                height="3"
-                xmlns="http://www.w3.org/2000/svg"
-                className="transition-all duration-300"
+              onClick={onClick} style={{verticalAlign: 'middle'}}
+        >
+            <svg width="16" height="3" xmlns="http://www.w3.org/2000/svg"
+                 className="transition-all duration-300 group-hover:w-[22px]"
             >
                 <circle cx="2" cy="1.5" r="1.5" fill="white"/>
                 <circle cx="8" cy="1.5" r="1.5" fill="white"/>
                 <circle cx="14" cy="1.5" r="1.5" fill="white"/>
 
-                <circle
-                    cx="20"
-                    cy="1.5"
-                    r="1.5"
-                    fill="white"
-                    className="scale-0 translate-x-[-6px] opacity-0 transition-all duration-300 origin-left group-hover:scale-100 group-hover:translate-x-0 group-hover:opacity-100"
+                <circle cx="20" cy="1.5" r="1.5" fill="white"
+                        className="scale-0 translate-x-[-6px] opacity-0 transition-all duration-300 origin-left group-hover:scale-100 group-hover:translate-x-0 group-hover:opacity-100"
                 />
             </svg>
         </span>
     );
-}
+};
 
 export default ShowMore;
