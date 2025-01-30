997ba689e7f78152ba6562088addc8151b1d280c
Christian Knapp <christian.alex.knapp@icloud.com>
2025-01-30 10:26:39 +0100

    simplified viaStops

--- src/app/components/scheduled.tsx
+++ src/app/components/scheduled.tsx
@@ -15,8 +15,9 @@ const ScheduledComponent: React.FC<ScheduledProps> = ({connection, isDeparture, 
     const [color, setColor] = useState<any>();
 
 	const [expandVia, setExpandVia] = useState(false);
-	const displayedViaStops = expandVia ? connection?.viaStops : connection?.viaStops.slice(0, 3);
-	const viaStops = displayedViaStops.map(writeName).join(" – ");
+	const viaStops = (expandVia ? connection?.viaStops : connection?.viaStops.slice(0, 3))
+        .map(stop => writeName(stop))
+        .join(" - ");
 
     const isDelayed = () => {
         const diff = calculateDuration(
