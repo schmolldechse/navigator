f5cd01f439a9ed5ff261f730e0a3ffa9a3ec4cfd
Christian Knapp <christian.alex.knapp@icloud.com>
2025-01-31 08:52:39 +0100

    remove shadcn - includes Sheet component - added Logo in TNavbar - improved design (#34)

--- src/app/components/timetable/TNavbar.tsx
+++ src/app/components/timetable/TNavbar.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname, useRouter } from "next/navigation";
-import { Menu } from "lucide-react";
+import Logo from "../branding/logo";
 
 interface Props {
     id: string
@@ -20,53 +19,21 @@ const TNavbar: React.FC<Props> = ({ id }) => {
     const handleNavigation = (path: string) => router.push(`/${id}${path}`);
 
     return (
-        <nav className="text-2xl p-4 w-full">
-            <div className="container mx-auto flex justify-between items-center">
-                {/* Home Link */}
-                <div
-                    className="hover:text-gray-400 pb-2 border-white cursor-pointer"
-                    onClick={() => router.push("/")}
-                >
-                    Home
-                </div>
+        <nav className="flex justify-between items-center text-xl montserrat-regular p-4 w-full">
+            {/* home */}
+            <Logo onClick={() => router.push("/")} />
 
-                {/* Desktop Navigation */}
-                <div className="hidden md:flex space-x-5">
-                    {navbarItems.map(({ label, path }) => (
-                        <span
-                            key={path}
-                            className={`hover:text-gray-400 pb-2 cursor-pointer ${pathname?.includes(path) ? 'border-b-2 border-white' : ''}`}
-                            onClick={() => handleNavigation(path)}
-                        >
-                            {label}
-                        </span>
-                    ))}
-                </div>
-
-                {/* Mobile Hamburger Menu */}
-                <div className="md:hidden">
-                    <Sheet>
-                        <SheetTrigger>
-                            <Menu className="w-8 h-8 cursor-pointer" />
-                        </SheetTrigger>
-                        <SheetContent className="w-[200px]">
-                            <SheetHeader>
-                                <SheetTitle></SheetTitle>
-                            </SheetHeader>
-                            <div className="mt-4 flex flex-col space-y-4">
-                                {navbarItems.map(({ label, path }) => (
-                                    <div
-                                        key={path}
-                                        className={`${pathname?.includes(path) ? 'font-bold' : ''}`}
-                                        onClick={() => handleNavigation(path)}
-                                    >
-                                        {label}
-                                    </div>
-                                ))}
-                            </div>
-                        </SheetContent>
-                    </Sheet>
-                </div>
+            {/* navigation */}
+            <div className="flex space-x-5">
+                {navbarItems.map(({ label, path }) => (
+                    <span
+                        key={path}
+                        className={`hover:text-gray-400 pb-0.5 cursor-pointer ${pathname?.includes(path) ? 'border-b-2 border-white' : ''}`}
+                        onClick={() => handleNavigation(path)}
+                    >
+                        {label}
+                    </span>
+                ))}
             </div>
         </nav>
     );
