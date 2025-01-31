fcf2ea6f191f0eba493399d5a18c4e3018d99257
Christian Knapp <christian.alex.knapp@icloud.com>
2025-01-31 21:51:24 +0100

    add cursor-pointer

--- src/app/components/branding/logo.tsx
+++ src/app/components/branding/logo.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Logo = ({ onClick }: Props) => {
     return (
-        <span className={"flex flex-row items-center montserrat-bold"} onClick={onClick}>
+        <span className={"flex flex-row items-center montserrat-bold cursor-pointer"} onClick={onClick}>
             <Image className={"ml-[10px]"} width={75} height={75} src={"/branding/logo.svg"} alt={"LOGO"} />
             <h1 className={"hidden md:inline text-[2rem] relative top-[-0.25rem]"}>NAVIGATOR</h1>
         </span>
