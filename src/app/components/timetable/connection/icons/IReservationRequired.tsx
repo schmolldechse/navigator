bdef8e403c696b91e3123665a3aca15ce4402c0b
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-02 12:44:43 +0100

    updated icons

--- src/app/components/timetable/connection/icons/IReservationRequired.tsx
+++ src/app/components/timetable/connection/icons/IReservationRequired.tsx
@@ -13,12 +13,9 @@ const IReservationRequired = ({width = 25, height = 25}: Props) => {
             width={width}
             className={"shrink-0"}
         >
-            <rect fill={"none"} stroke={"#fff"} strokeMiterlimit={"10"} strokeWidth={"10x"} x="48.14" y="62.93"
-                  width="945.91" height="945.91" rx="50" ry="50"/>
-            <text fill={"#fff"} fontFamily={"Calibri, Calibri"} fontSize={"1000px"}
-                  transform="translate(247.56 853.33)">
-                <tspan x="0" y="0">R</tspan>
-            </text>
+            <rect fill={"none"} stroke={"#fff"} strokeMiterlimit={"10"} strokeWidth={"30px"} x="48.14" y="62.93" width="945.91" height="945.91" rx="50" ry="50"/>
+            <path fill={"#fff"}
+                  d="M596.44,589.68c71.39-21.38,123.7-69.87,128.23-186.16-2.29-129.95-72.52-193.21-204.27-196.1h-235.39v656.94h97.1v-264.75h109.91l155.7,264.75h111.76l-163.05-274.68ZM382.11,520.99v-229.52h139.19c71.29.86,104.82,41.09,106.26,114.76-.41,73.19-34.46,112.81-106.26,114.76h-139.19Z"/>
         </svg>
     );
 };
