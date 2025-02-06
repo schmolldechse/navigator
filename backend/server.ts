36ae22bb970cdc90029c40cb4ea30a57f26e6a4b
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-06 14:20:25 +0100

    backend: add cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,12 @@
 // @ts-types="npm:@types/express"
 import express from "npm:express";
+import cors from "npm:cors";
 import router from "./router.ts";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors("*"));
 app.use(router);
 
 app.listen(8000, () => console.log("Server is running on port 8000"));
