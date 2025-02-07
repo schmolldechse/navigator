b60539ceaccdb606b3981dc1cb4037305f9e09f1
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-07 15:31:22 +0100

    WIP frontend: added now button - align items on the same axis - added svg for now button

--- frontend/src/components/timetable/TimeTable.svelte
+++ frontend/src/components/timetable/TimeTable.svelte
@@ -3,6 +3,7 @@
 	import StationSearch from "$components/StationSearch.svelte";
 	import type { Station } from "$models/station";
 	import TimePicker from "$components/timetable/TimePicker.svelte";
+	import Clock from "$components/svg/Clock.svelte";
 
 	let typeSelected: "departures" | "arrivals" = $state("departures");
 
@@ -48,17 +49,24 @@
 
 	<StationSearch onStationSelect={(station) => (stationSelected = station)} />
 
-	<div class="flex flex-col">
-		<span>Pick a time:</span>
-		<TimePicker onChangedDate={(newDate) => (dateSelected = newDate)} />
-	</div>
+    <div class="flex flex-row justify-between">
+        <TimePicker onChangedDate={(newDate) => (dateSelected = newDate)}/>
 
-	<button
-		class="{stationSelected && dateSelected
+        <div class="flex flex-row">
+            <button class="flex flex-row items-center bg-primary rounded-3xl px-2">
+                <Clock height="25px" width="25px" />
+                <span class="text-xl">Now</span>
+            </button>
+
+            <button
+                    class="{stationSelected && dateSelected
 			? 'bg-accent text-black'
-			: 'bg-primary text-text'} flex items-center justify-center rounded-md p-2 text-base font-bold text-background md:text-2xl"
-		onclick={gotoRequest}
-	>
-		Request
-	</button>
+			: 'bg-primary text-text'} flex items-center justify-center rounded-3xl px-4 text-base font-bold text-background md:text-2xl"
+                    onclick={gotoRequest}
+            >
+                Search
+            </button>
+        </div>
+    </div>
+
 </div>
