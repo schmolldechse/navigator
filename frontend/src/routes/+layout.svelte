c178b611a0528708eaccba274eee262e5a8a1dd6
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-07 20:37:48 +0100

    frontend: removed margin, semantic adjustments

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -7,22 +7,20 @@
 	let { children } = $props();
 
 	let type = $state("timetable");
-
 	setContext("type", () => type);
 </script>
 
-<div class="m-4">
-	<div class="mb-4 flex items-center justify-between bg-accent pr-4 dark:bg-transparent">
-		<!-- Logo -->
-		<a href="/">
-			<span class="flex cursor-pointer flex-row items-center font-bold">
-				<Logo />
-				<h1 class="relative top-[-0.25rem] hidden text-[2rem] md:inline">NAVIGATOR</h1>
-			</span>
-		</a>
+<!-- Logo/ Navbar -->
+<div class="flex items-center justify-between bg-accent pr-4 dark:bg-transparent">
+	<a href="/">
+		<div class="flex cursor-pointer flex-row items-center">
+			<Logo />
+			<h1 class="relative top-[-0.25rem] hidden md:inline text-[2rem] font-bold">NAVIGATOR</h1>
+		</div>
+	</a>
 
-		<Navbar bind:type />
-	</div>
-
-	{@render children()}
+	<Navbar bind:type />
 </div>
+
+<!-- Content -->
+{@render children()}
