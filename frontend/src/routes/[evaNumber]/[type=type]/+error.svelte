144a8b0e983e23c92305f856c14594769053fa9c
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-10 19:00:27 +0100

    frontend: add +error.svelte when station does not exist

---
+++ frontend/src/routes/[evaNumber]/[type=type]/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from "$app/state";
+</script>
+
+<div class="flex items-center justify-center align-middle">
+	<div>
+		<p class="text-7xl font-bold">
+			{page.status}
+		</p>
+		<p class="text-2xl font-medium">This station does not exist. Maybe you want to try a different?</p>
+	</div>
+</div>
