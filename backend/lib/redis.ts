2d8ffe52514a8e1111332281a82ba93245dabd22
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-11 20:17:08 +0100

    backend: add missing database parameter

--- backend/lib/redis.ts
+++ backend/lib/redis.ts
@@ -4,13 +4,14 @@ const host: string = process.env.REDIS_HOST || "localhost";
 const port: number = Number.parseInt(process.env.REDIS_PORT ?? "") || 6379;
 const user: string = process.env.REDIS_USER || "";
 const password: string = process.env.REDIS_PASSWORD || "";
+const database: number = Number.parseInt(process.env.REDIS_DATABASE ?? "") || 0;
 
 let client: RedisClientType | null = null;
 
 export const getRedisClient = async (): Promise<RedisClientType> => {
 	if (client) return client;
 
-	const redisUrl = `redis://${user}:${password}@${host}:${port}`;
+	const redisUrl = `redis://${user}:${password}@${host}:${port}/${database}`;
 
 	client = createClient({ url: redisUrl });
 	client.on("error", (err) => console.error("Redis client error: ", err));
