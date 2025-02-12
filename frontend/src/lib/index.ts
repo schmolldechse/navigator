c979262bee6b1ed4d01ba2a777d318e39bf29d45
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-12 14:43:02 +0100

    WIP frontend: add writeStop method

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,8 +1,15 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { goto } from "$app/navigation";
+import type {Stop} from "$models/station";
 
 const gotoTimetable = (evaNumber: string, type: "departures" | "arrivals", startDate: string) => {
 	goto(`/${evaNumber}/${type}?startDate=${encodeURIComponent(startDate)}`);
 };
 
-export { gotoTimetable };
+const writeStop = (stop: Stop, fallbackName: string = ""): string => {
+	if (!stop.name) return fallbackName;
+	if (!stop.nameParts || stop.nameParts.length === 0) return stop.name;
+	return stop.nameParts.map(part => part.value).join("").trim();
+}
+
+export { gotoTimetable, writeStop };
