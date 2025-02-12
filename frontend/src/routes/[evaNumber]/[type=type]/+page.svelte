1a2263f73481289c415de3fb878eb0bbe056160b
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-12 20:57:26 +0100

    frontend: fixed finally Filter being at the bottom?´!?!?!?

--- frontend/src/routes/[evaNumber]/[type=type]/+page.svelte
+++ frontend/src/routes/[evaNumber]/[type=type]/+page.svelte
@@ -63,10 +63,10 @@
 		{/each}
 	</div>
 
-    <div class="fixed md:block w-full bottom-0 left-0 right-0 z-50">
-        <Filter allowedProducts={data.station.products ? Object.values(data.station.products) : []}
-                bind:selected={currentFilter}/>
-    </div>
+	<div class="self-end w-full bottom-0">
+		<Filter allowedProducts={data.station.products ? Object.values(data.station.products) : []}
+				bind:selected={currentFilter} />
+	</div>
 </div>
 
 <style>
