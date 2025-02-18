43baf5c81c7462b602163cab925ceb328ceeda4a
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-18 19:03:43 +0100

    frontend: add title

--- frontend/src/components/sequence/Sequence.svelte
+++ frontend/src/components/sequence/Sequence.svelte
@@ -63,59 +63,64 @@
 	};
 </script>
 
-<div class="h-full max-w-full content-end overflow-x-auto rounded-lg bg-primary-darker pb-4 md:px-16">
-	<VehicleInfo track={sequence.track} vehicle={vehicleById} />
+<div class="h-full max-w-full flex flex-col overflow-x-auto rounded-lg bg-primary-darker pb-4 md:px-16">
+	<span class="text-3xl font-bold pt-8">Coach Sequence</span>
 
-	<!-- Track Visualization -->
-	<div class="relative mt-12 w-full min-w-[800px]">
-		{#each sequence.track.sections as section}
-			{@const style = calculateLength(section.start.position, section.end.position)}
-			<div class="absolute flex h-full items-center justify-center" style="width: {style.width}; left: {style.left}">
-				<span class="font-medium">{section.name}</span>
-			</div>
-		{/each}
-	</div>
+	<div class="h-full max-w-full content-end">
+		<VehicleInfo track={sequence.track} vehicle={vehicleById} />
 
-	<!-- Vehicle Groups Visualization -->
-	{#if sequence.vehicleGroup}
-		<div class="relative h-20 w-full min-w-[800px]">
-			{#each sequence.vehicleGroup as group, groupIndex (group)}
-				<!-- Render each vehicle in its exact position on the track -->
-				{#each group.vehicles as vehicle, vehicleIndex (vehicle)}
-					{@const style = calculateLength(
-						vehicle.positionOnTrack.start.position,
-						vehicle.positionOnTrack.end.position
-					)}
-					{@const vehicleType = vehicle.vehicleType}
-					{@const isSelected =
-						selectedVehicle.groupIndex === groupIndex && selectedVehicle.vehicleIndex === vehicleIndex}
-					<button
-						class="absolute h-full cursor-pointer transition-transform hover:scale-110"
-						style="width: {style.width}; left: {style.left}; min-width: {style.minWidth}"
-						class:scale-110={isSelected}
-						onclick={() => selectVehicle(groupIndex, vehicleIndex)}
-					>
-						{#if vehicleType.category.includes("LOCOMOTIVE")}
-							<Locomotive />
-						{:else if hasLocomotive(vehicle, "before")}
-							<CoachStart firstClass={vehicleType.firstClass} />
-						{:else if hasLocomotive(vehicle, "after")}
-							<CoachEnd firstClass={vehicleType.firstClass} />
-						{:else if vehicleIndex === 0}
-							<ControlcarEnd firstClass={vehicleType.firstClass} />
-						{:else if vehicleIndex === group.vehicles.length - 1}
-							<ControlcarStart firstClass={vehicleType.firstClass} />
-						{:else}
-							<CoachMiddle firstClass={vehicleType.firstClass} />
-						{/if}
-					</button>
-				{/each}
+		<!-- Track Visualization -->
+		<div class="relative mt-12 w-full min-w-[800px]">
+			{#each sequence.track.sections as section}
+				{@const style = calculateLength(section.start.position, section.end.position)}
+				<div class="absolute flex h-full items-center justify-center"
+					 style="width: {style.width}; left: {style.left}">
+					<span class="font-medium">{section.name}</span>
+				</div>
 			{/each}
 		</div>
-	{/if}
 
-	<div class="mt-4 flex flex-col">
-		<span class="text-text">{tripReference().referenceId}</span>
-		<span class="font-semibold">{tripReference().destination}</span>
+		<!-- Vehicle Groups Visualization -->
+		{#if sequence.vehicleGroup}
+			<div class="relative h-20 w-full min-w-[800px]">
+				{#each sequence.vehicleGroup as group, groupIndex (group)}
+					<!-- Render each vehicle in its exact position on the track -->
+					{#each group.vehicles as vehicle, vehicleIndex (vehicle)}
+						{@const style = calculateLength(
+							vehicle.positionOnTrack.start.position,
+							vehicle.positionOnTrack.end.position
+						)}
+						{@const vehicleType = vehicle.vehicleType}
+						{@const isSelected =
+						selectedVehicle.groupIndex === groupIndex && selectedVehicle.vehicleIndex === vehicleIndex}
+						<button
+							class="absolute h-full cursor-pointer transition-transform hover:scale-110"
+							style="width: {style.width}; left: {style.left}; min-width: {style.minWidth}"
+							class:scale-110={isSelected}
+							onclick={() => selectVehicle(groupIndex, vehicleIndex)}
+						>
+							{#if vehicleType.category.includes("LOCOMOTIVE")}
+								<Locomotive />
+							{:else if hasLocomotive(vehicle, "before")}
+								<CoachStart firstClass={vehicleType.firstClass} />
+							{:else if hasLocomotive(vehicle, "after")}
+								<CoachEnd firstClass={vehicleType.firstClass} />
+							{:else if vehicleIndex === 0}
+								<ControlcarEnd firstClass={vehicleType.firstClass} />
+							{:else if vehicleIndex === group.vehicles.length - 1}
+								<ControlcarStart firstClass={vehicleType.firstClass} />
+							{:else}
+								<CoachMiddle firstClass={vehicleType.firstClass} />
+							{/if}
+						</button>
+					{/each}
+				{/each}
+			</div>
+		{/if}
+
+		<div class="mt-4 flex flex-col">
+			<span class="text-text">{tripReference().referenceId}</span>
+			<span class="font-semibold">{tripReference().destination}</span>
+		</div>
 	</div>
 </div>
