7942c064611c758a968f54491439bb852cabd139
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-28 18:06:24 +0100

    feat(frontend): new login design

--- frontend/src/routes/login/+layout.svelte
+++ frontend/src/routes/login/+layout.svelte
@@ -1,12 +1,42 @@
 <script lang="ts">
 	import "$src/app.css";
-	import Navbar from "$components/Navbar.svelte";
 	import type { LayoutProps } from "./$types";
+	import { MetaTags } from "svelte-meta-tags";
+	import LogoIcon from "$components/branding/LogoIcon.svelte";
 
 	let { children }: LayoutProps = $props();
 </script>
 
-<div class="flex h-screen w-screen flex-col overflow-hidden">
-	<Navbar userInfo={false} />
-	{@render children()}
+<MetaTags
+	title="Navigator"
+	description="Login to continue"
+	openGraph={{
+		url: "https://navigator.voldechse.wtf/login",
+		title: "Navigator",
+		siteName: "Navigator",
+		description: "Login to continue",
+		images: [
+			{
+				url: "https://navigator.voldechse.wtf/logo.png",
+				width: 1024,
+				height: 1024,
+				alt: "Navigator Logo"
+			}
+		]
+	}}
+/>
+
+<div class="flex flex-col h-screen container mx-auto items-center gap-y-4 pt-24 md:pt-48">
+	<div class="flex justify-center">
+		<LogoIcon height="150" width="150" />
+	</div>
+
+	<div class="md:w-[40%] space-y-12">
+		<div class="flex flex-col">
+			<span class="font-bold text-3xl">Login</span>
+			<span>Please login to continue.</span>
+		</div>
+
+		{@render children()}
+	</div>
 </div>
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,36 +1,12 @@
 <script lang="ts">
 	import GitHub from "$components/ui/icons/GitHub.svelte";
 	import { authClient } from "$lib/auth-client";
-	import { MetaTags } from "svelte-meta-tags";
 </script>
 
-<MetaTags
-	title="Navigator"
-	description="The Navigator for your train journeys."
-	openGraph={{
-		url: "https://navigator.voldechse.wtf",
-		title: "Navigator",
-		siteName: "Navigator",
-		description: "The Navigator for your train journeys.",
-		images: [
-			{
-				url: "https://navigator.voldechse.wtf/logo.png",
-				width: 1024,
-				height: 1024,
-				alt: "Navigator Logo"
-			}
-		]
-	}}
-/>
-
-<div
-	class="bg-primary-darker mb-8 flex w-full flex-1 flex-col items-center justify-center self-center rounded-3xl p-4 md:w-2/4"
+<button
+	class="w-full bg-primary-darker hover:bg-primary-dark cursor-pointer flex flex-row items-center justify-center gap-x-4 rounded-l p-2"
+	onclick={() => authClient.signIn.social({ provider: "github", callbackURL: window.location.origin })}
 >
-	<button
-		class="bg-primary-dark hover:bg-primary flex flex-row items-center gap-x-4 rounded-l p-4"
-		onclick={() => authClient.signIn.social({ provider: "github", callbackURL: window.location.origin })}
-	>
-		<GitHub height="35px" width="35px" />
-		<span class="text-xl font-medium">Continue with GitHub</span>
-	</button>
-</div>
+	<GitHub height="25" width="25" />
+	<span class="text-lg font-medium">Continue with GitHub</span>
+</button>
