5e10442821b015af9d0e8cea4dd3f16c32b012a3
Christian Knapp <christian.alex.knapp@icloud.com>
2025-03-02 16:26:02 +0100

    chore(backend): switched to drizzle orm

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,48 @@
+import { pgTable, text, timestamp, boolean } from 'drizzle-orm/pg-core';
+
+export const user = pgTable("user", {
+	id: text("id").notNull().primaryKey(),
+	name: text("name").notNull(),
+	email: text("email").notNull().unique(),
+	emailVerified: boolean("emailVerified").notNull(),
+	image: text("image"),
+	createdAt: timestamp("createdAt", { precision: 3 }).notNull(),
+	updatedAt: timestamp("updatedAt", { precision: 3 }).notNull(),
+	username: text("username").notNull().unique(),
+});
+
+export const session = pgTable("session", {
+	id: text("id").notNull().primaryKey(),
+	expiresAt: timestamp("expiresAt", { precision: 3 }).notNull(),
+	token: text("token").notNull().unique(),
+	createdAt: timestamp("createdAt", { precision: 3 }).notNull(),
+	updatedAt: timestamp("updatedAt", { precision: 3 }).notNull(),
+	ipAddress: text("ipAddress"),
+	userAgent: text("userAgent"),
+	userId: text("userId").notNull().references(() => user.id),
+});
+
+export const account = pgTable("account", {
+	id: text("id").notNull().primaryKey(),
+	accountId: text("accountId").notNull(),
+	providerId: text("providerId").notNull(),
+	userId: text("userId").notNull().references(() => user.id),
+	accessToken: text("accessToken"),
+	refreshToken: text("refreshToken"),
+	idToken: text("idToken"),
+	accessTokenExpiresAt: timestamp("accessTokenExpiresAt", { precision: 3 }),
+	refreshTokenExpiresAt: timestamp("refreshTokenExpiresAt", { precision: 3 }),
+	scope: text("scope"),
+	password: text("password"),
+	createdAt: timestamp("createdAt", { precision: 3 }).notNull(),
+	updatedAt: timestamp("updatedAt", { precision: 3 }).notNull(),
+});
+
+export const verification = pgTable("verification", {
+	id: text("id").notNull().primaryKey(),
+	identifier: text("identifier").notNull(),
+	value: text("value").notNull(),
+	expiresAt: timestamp("expiresAt", { precision: 3 }).notNull(),
+	createdAt: timestamp("createdAt", { precision: 3 }),
+	updatedAt: timestamp("updatedAt", { precision: 3 }),
+});
--- backend/src/lib/auth/auth.ts
+++ backend/src/lib/auth/auth.ts
@@ -1,17 +1,24 @@
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "../database/prisma.ts";
 import { betterAuth } from "better-auth";
 import { usernamePlugin } from "../plugins/username.ts";
 import type { GithubProfile } from "better-auth/social-providers";
+import { Pool } from "pg";
 
 export const auth = betterAuth({
-	database: prismaAdapter(prisma, { provider: "postgresql" }),
+	database: new Pool({
+		connectionString: process.env.AUTH_POSTGRES_URL!,
+	}),
 	socialProviders: {
 		github: {
 			enabled: true,
 			clientId: process.env.GITHUB_CLIENT_ID!,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET!,
 			mapProfileToUser: (profile: GithubProfile) => ({ username: profile.login })
+		}
+	},
+	session: {
+		cookieCache: {
+			enabled: true,
+			maxAge: 5 * 60
 		}
 	},
 	basePath: "/auth",
