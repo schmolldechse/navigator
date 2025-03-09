c99f52535d06bf5e4e53b2f0a51ccf6849289daf
Christian Knapp <christian.alex.knapp@icloud.com>
2025-03-09 09:35:27 +0100

    feat(daemon): adjust LastQueried if not available / behind current date

--- Daemon/System/Impl/MonitorStations.cs
+++ Daemon/System/Impl/MonitorStations.cs
@@ -1,5 +1,4 @@
 using Daemon.Models;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Daemon.System.Impl;
@@ -12,15 +11,53 @@ public class MonitorStations : Daemon
     
     protected override async Task ExecuteCoreAsync(CancellationToken cancellationToken)
     {
+        var date = DateTime.Now;
+
         var collection = await MongoDriver.GetCollectionAsync<StationDocument>("stations");
-        var filter = Builders<StationDocument>.Filter.Eq(x => x.QueryingEnabled, true);
+
+        var queryingEnabledFilter = Builders<StationDocument>.Filter.Eq(x => x.QueryingEnabled, true);
+        var lastQueriedFilter = Builders<StationDocument>.Filter.Or(
+            Builders<StationDocument>.Filter.Exists(x => x.LastQueried, false),
+            Builders<StationDocument>.Filter.Lt(x => x.LastQueried, date)
+        );
+
         var station = await collection
             .Aggregate()
-            .Match(filter)
+            .Match(Builders<StationDocument>.Filter.And(queryingEnabledFilter, lastQueriedFilter))
             .Sample(1)
             .FirstOrDefaultAsync(cancellationToken);
         if (station == null) return;
 
-        Console.WriteLine("Monitoring station: " + station.ToJson());
+        if (station.LastQueried == null)
+        {
+            // RIS saves it up to 7 days retroactively
+            var start = date.AddDays(-7);
+            var update = Builders<StationDocument>.Update.Set(x => x.LastQueried, start);
+
+            await collection.UpdateOneAsync(
+                Builders<StationDocument>.Filter.Eq(x => x.EvaNumber, station.EvaNumber),
+                update,
+                cancellationToken: cancellationToken
+            );
+            
+            Console.WriteLine($"Updated 'LastQueried' for station {station.Name} to 7 days ago: {start}");
+        }
+        else if (station.LastQueried.Value.Date < date.Date)
+        {
+            var newLastQueried = new DateTime(
+                DateOnly.FromDateTime(station.LastQueried.Value.Date.AddDays(1)),
+                TimeOnly.FromTimeSpan(date.TimeOfDay)
+            );
+
+            var update = Builders<StationDocument>.Update.Set(x => x.LastQueried, newLastQueried);
+
+            await collection.UpdateOneAsync(
+                Builders<StationDocument>.Filter.Eq(x => x.EvaNumber, station.EvaNumber),
+                update,
+                cancellationToken: cancellationToken
+            );
+
+            Console.WriteLine($"Incremented 'LastQueried' for station {station.Name} to {newLastQueried}");
+        }
     }
 }
