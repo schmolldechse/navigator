5ad8031e28556e35431f500ffca7f770562b5c31
Christian Knapp <christian.alex.knapp@icloud.com>
2025-03-13 22:05:24 +0100

    refactor(navbar): added hamburger icon, changed image size

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import VisualIdentity from "$components/branding/VisualIdentity.svelte";
 	import UserInfo from "$components/auth/UserInfo.svelte";
+	import Menu from "lucide-svelte/icons/menu";
 
 	let { userInfo = true, dropdown = false }: { userInfo?: boolean, dropdown?: boolean } = $props();
 </script>
@@ -8,12 +9,12 @@
 <div class="bg-background sticky top-0 flex h-20 w-full items-center justify-between px-2 pt-4 pr-4">
 	<VisualIdentity />
 
-	<div class="flex flex-row items-center">
+	<div class="flex flex-row items-center gap-x-4">
 		{#if userInfo}
 			<UserInfo />
 		{/if}
 		{#if dropdown}
-			<span>hey!</span>
+			<Menu size={35} />
 		{/if}
 	</div>
 </div>
