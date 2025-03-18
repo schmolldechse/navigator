1894089bb31839d54d78f79490b376fcdc67181f
Christian Knapp <christian.alex.knapp@icloud.com>
2025-03-18 21:23:21 +0100

    fix(timeinformation): align "Minus" icon correctly

--- frontend/src/components/route-planner/Route.svelte
+++ frontend/src/components/route-planner/Route.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import type { Route } from "$models/route";
-	import TimeInformation from "$components/ui/info/TimeInformation.svelte";
 	import Minus from "lucide-svelte/icons/minus";
+	import TimeInformation from "$components/ui/info/TimeInformation.svelte";
 
 	let { route }: { route: Route } = $props();
 </script>
 
-<div class="text-2xl flex flex-row items-start">
+<div class="text-2xl font-medium flex flex-row">
 	<TimeInformation time={route?.legs[0]?.departure} direction="col" />
-	<Minus />
+	<Minus class="mx-2 mt-[0.25rem]" />
 	<TimeInformation time={route?.legs[route?.legs?.length - 1]?.arrival} direction="col" />
+
+	<span>hey</span>
 </div>
--- frontend/src/components/ui/info/TimeInformation.svelte
+++ frontend/src/components/ui/info/TimeInformation.svelte
@@ -9,14 +9,15 @@
 	const displayTime = (time: string) => DateTime.fromISO(time).setLocale("de-DE").toLocaleString(DateTime.TIME_24_SIMPLE);
 </script>
 
-<div class="flex items-center justify-end gap-x-2"
+<div class="flex"
 	 class:flex-row={direction === "row"}
+	 class:items-center={direction === "row"}
+	 class:justify-end={direction === "row"}
+	 class:gap-x-2={direction === "row"}
 	 class:flex-col={direction === "col"}
 >
-	<span
-		class:px-2={direction === "col"}
-	>{displayTime(time?.plannedTime ?? "")}</span>
+	<span>{displayTime(time?.plannedTime ?? "")}</span>
 	{#if isDelayed()}
-		<span class="bg-text text-background px-2 text-lg font-bold md:py-0.25 md:text-2xl">{displayTime(time?.actualTime ?? "")}</span>
+		<span class:md:text-2xl={direction === "row"} class="text-center bg-text text-background px-2 text-lg font-bold">{displayTime(time?.actualTime ?? "")}</span>
 	{/if}
 </div>
