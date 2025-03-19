9f085b1cc891e32e11b4de76429c7988618f0d8e
Christian Knapp <christian.alex.knapp@icloud.com>
2025-03-19 22:07:17 +0100

    feat(route planner): parse duration

--- frontend/src/components/route-planner/Route.svelte
+++ frontend/src/components/route-planner/Route.svelte
@@ -2,14 +2,38 @@
 	import type { Route } from "$models/route";
 	import Minus from "lucide-svelte/icons/minus";
 	import TimeInformation from "$components/ui/info/TimeInformation.svelte";
+	import calculateDuration from "$lib/time";
+	import { DateTime } from "luxon";
 
 	let { route }: { route: Route } = $props();
+
+	const formatDuration = (): string => {
+		const duration = calculateDuration(
+			DateTime.fromISO(route?.legs[route?.legs?.length - 1]?.arrival?.plannedTime ?? route?.legs[route?.legs?.length - 1]?.arrival?.actualTime ?? ""),
+			DateTime.fromISO(route?.legs[0]?.departure?.plannedTime ?? route?.legs[0]?.departure?.actualTime ?? ""),
+			["minutes"]
+		);
+		const total = duration.as("minutes");
+
+		if (total > 60) {
+			const hours = Math.floor(total / 60);
+			const minutes = total % 60;
+			return `${hours} h ${minutes} min`;
+		} else return `${Math.floor(total)} min`;
+	};
 </script>
 
-<div class="text-2xl font-medium flex flex-row">
-	<TimeInformation time={route?.legs[0]?.departure} direction="col" />
-	<Minus class="mx-2 mt-[0.25rem]" />
-	<TimeInformation time={route?.legs[route?.legs?.length - 1]?.arrival} direction="col" />
+<div class="text-2xl font-medium">
+	<!-- Time Info -->
+	<div class="flex flex-row gap-x-2">
+		<div class="flex flex-row ">
+			<TimeInformation time={route?.legs[0]?.departure} direction="col" />
+			<Minus class="mx-2 mt-[0.25rem]" />
+			<TimeInformation time={route?.legs[route?.legs?.length - 1]?.arrival} direction="col" />
+		</div>
 
-	<span>hey</span>
+		<span class="text-primary/90">|</span>
+		<span
+			class="text-lg mt-[0.35rem]">{formatDuration()}</span>
+	</div>
 </div>
--- frontend/src/lib/time.ts
+++ frontend/src/lib/time.ts
@@ -1,14 +1,14 @@
-import type { DateTime, DurationObjectUnits, DurationUnit } from "luxon";
+import { type DateTime, Duration, type DurationUnit } from "luxon";
 
 const calculateDuration = (
 	startDate: DateTime,
 	endDate: DateTime,
 	unit: DurationUnit | DurationUnit[]
-): DurationObjectUnits => {
+): Duration => {
 	if (!startDate.isValid || !endDate.isValid) {
 		throw new Error("Invalid DateTime objects provided");
 	}
-	return startDate.diff(endDate, unit).toObject();
+	return startDate.diff(endDate, unit);
 };
 
 export default calculateDuration;
