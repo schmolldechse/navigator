158a4ec3711182c0633580543a0b5412305c1243
Christian Knapp <christian.alex.knapp@icloud.com>
2025-03-22 10:45:11 +0100

    feat(route-planner): display route legs

--- frontend/src/components/route-planner/Route.svelte
+++ frontend/src/components/route-planner/Route.svelte
@@ -4,13 +4,17 @@
 	import TimeInformation from "$components/ui/info/TimeInformation.svelte";
 	import calculateDuration from "$lib/time";
 	import { DateTime } from "luxon";
+	import type { Connection } from "$models/connection";
 
 	let { route }: { route: Route } = $props();
 
 	const formatDuration = (): string => {
+		const end = route?.legs[route?.legs?.length - 1];
+		const start = route?.legs[0];
+
 		const duration = calculateDuration(
-			DateTime.fromISO(route?.legs[route?.legs?.length - 1]?.arrival?.plannedTime ?? route?.legs[route?.legs?.length - 1]?.arrival?.actualTime ?? ""),
-			DateTime.fromISO(route?.legs[0]?.departure?.plannedTime ?? route?.legs[0]?.departure?.actualTime ?? ""),
+			DateTime.fromISO(end?.arrival?.actualTime ?? end?.arrival?.plannedTime ?? ""),
+			DateTime.fromISO(start?.departure?.actualTime ?? start?.departure?.plannedTime ?? ""),
 			["minutes"]
 		).as("minutes");
 
@@ -20,9 +24,35 @@
 			return `${hours} h ${minutes} min`;
 		} else return `${Math.floor(duration)} min`;
 	};
+
+	const durationWithoutWalking = $derived(() => {
+		// remove legs with "walking" property
+		const legs = (route?.legs || []).filter(leg => !leg?.walking);
+		if (legs.length === 0) return 0;
+
+		return legs.flatMap(leg => calculateDuration(
+				DateTime.fromISO(leg?.arrival?.actualTime ?? leg?.arrival?.plannedTime ?? ""),
+				DateTime.fromISO(leg?.departure?.actualTime ?? leg?.departure?.plannedTime ?? ""),
+				["minutes"]
+			).as("minutes"))
+			.reduce((acc, el) => acc + el);
+	});
+
+	const durationByConnection = (connection: Connection) => {
+		return calculateDuration(
+			DateTime.fromISO(connection?.arrival?.actualTime ?? connection?.arrival?.plannedTime ?? ""),
+			DateTime.fromISO(connection?.departure?.actualTime ?? connection?.departure?.plannedTime ?? ""),
+			["minutes"]
+		).as("minutes");
+	}
+
+	const getWidthRatio = (duration: number, maxDuration: number) => {
+		if (maxDuration <= 0) return '0%';
+		return `${(duration / maxDuration) * 100}%;`
+	}
 </script>
 
-<div class="text-2xl font-medium">
+<div class="px-4 py-2 text-2xl font-medium space-y-2 border-primary-dark/75 border-2 rounded-lg">
 	<!-- Time Info -->
 	<div class="flex flex-row gap-x-2">
 		<div class="flex flex-row ">
@@ -34,5 +64,11 @@
 		<span class="text-primary/90">|</span>
 		<span
 			class="text-lg mt-[0.35rem]">{formatDuration()}</span>
+	</div>
+
+	<div class="flex flex-row w-full gap-x-2">
+		{#each route?.legs.filter(leg => !leg?.walking) as leg}
+			<span class="rounded-lg bg-primary-darker text-base md:text-lg px-2 py-1 text-center line-clamp-1 md:line-clamp-none truncate" style={`width: ${getWidthRatio(durationByConnection(leg), durationWithoutWalking())}`}>{leg?.lineInformation?.lineName}</span>
+		{/each}
 	</div>
 </div>
