6b789b96ddee48f152617310c538ba1b190fcf8e
Christian Knapp <christian.alex.knapp@icloud.com>
2025-03-29 15:28:51 +0100

    refactor(platform component): remove clsx (see dc216d2)

--- frontend/src/components/ui/info/Platform.svelte
+++ frontend/src/components/ui/info/Platform.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import type { Time } from "$models/time";
-	import clsx from "clsx";
 
 	let { time, class: className = "", direction = "row" }: {
 		time?: Time,
@@ -12,14 +11,14 @@
 </script>
 
 {#if direction === "row"}
-<span class={clsx("md:w-full", {"bg-text text-background px-2 py-1 md:px-0": !samePlatform}, className)}>
+<span class={["md:w-full", {"bg-text text-background px-2 py-1 md:px-0": !samePlatform}, className]}>
 	{!samePlatform ? time?.actualPlatform : time?.plannedPlatform}
 </span>
 {:else}
-	<div class={clsx("flex flex-col md:w-full items-end", className)}>
+	<div class={["flex flex-col md:w-full items-end", className]}>
 		<span>{time?.plannedPlatform ?? ""}</span>
 		{#if !samePlatform}
-			<span class={clsx("font-bold w-fit bg-text text-background px-2")}>{time?.actualPlatform ?? ""}</span>
+			<span class="font-bold w-fit bg-text text-background px-2">{time?.actualPlatform ?? ""}</span>
 		{/if}
 	</div>
 {/if}
