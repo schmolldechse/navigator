dc216d29e99bae7eced79d6809e03d8a81c7f9d8
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2025-03-29 15:18:59 +0100

    fix(frontend): use svelte syntax instead of clsx

--- frontend/src/components/ui/info/TimeInformation.svelte
+++ frontend/src/components/ui/info/TimeInformation.svelte
@@ -2,13 +2,17 @@
 	import type { Time } from "$models/time";
 	import calculateDuration from "$lib/time";
 	import { DateTime } from "luxon";
-	import clsx from "clsx";
 
-	let { time, direction = "row", class: className = "", delayClass = "" }: {
+	let {
+		time,
+		direction = "row",
+		class: className = "",
+		delayClass = ""
+	}: {
 		time?: Time;
-		direction?: "row" | "col",
-		class?: string,
-		delayClass?: string
+		direction?: "row" | "col";
+		class?: string;
+		delayClass?: string;
 	} = $props();
 
 	const isDelayed = () =>
@@ -19,13 +23,17 @@
 </script>
 
 <div
-	class={clsx("flex", {
-		"flex-row items-center justify-end gap-x-2": direction === "row",
-		"flex-col": direction === "col"
-	}, className)}
+	class={[
+		"flex",
+		{ "flex-row items-center justify-end gap-x-2": direction === "row" },
+		{ "flex-col": direction === "col" },
+		className
+	]}
 >
 	<span>{displayTime(time?.plannedTime ?? "")}</span>
 	{#if isDelayed()}
-		<span class={clsx("w-fit bg-text text-background px-2 text-center font-bold", delayClass)}>{displayTime(time?.actualTime ?? "")}</span>
+		<span class={["bg-text text-background w-fit px-2 text-center font-bold", delayClass]}
+			>{displayTime(time?.actualTime ?? "")}</span
+		>
 	{/if}
 </div>
