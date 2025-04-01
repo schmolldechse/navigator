298d2b6c58f9b15ad6d682ea7f691aa68f978e93
Christian Knapp <christian.alex.knapp@icloud.com>
2025-04-01 23:14:51 +0200

    refactor(changeover component): adjusted min height

--- frontend/src/components/route-planner/Changeover.svelte
+++ frontend/src/components/route-planner/Changeover.svelte
@@ -6,7 +6,7 @@
 	let { arrival, departure }: { arrival?: Time; departure?: Time } = $props();
 </script>
 
-<div class="flex min-h-24 flex-row text-base">
+<div class="flex min-h-32 flex-row text-base">
 	<span class="text-text/65 basis-1/5 self-center text-right">{formatDuration(departure, arrival)}</span>
 	<div class="relative flex basis-[15%] justify-center md:basis-[5%]">
 		<span
