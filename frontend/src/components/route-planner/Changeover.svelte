5a25cecc3f1158f8b1f56b35a4071db6bcce4055
Christian Knapp <christian.alex.knapp@icloud.com>
2025-04-02 18:21:29 +0200

    feat(changeover, #103): add visual warning for impossible changover times

--- frontend/src/components/route-planner/Changeover.svelte
+++ frontend/src/components/route-planner/Changeover.svelte
@@ -1,20 +1,41 @@
 <script lang="ts">
-	import { formatDuration } from "$lib";
+	import { calculateDuration, formatDuration } from "$lib";
 	import type { Time } from "$models/time";
 	import Walking from "$components/ui/icons/Walking.svelte";
+	import { DateTime } from "luxon";
+	import CancelledTrip from "$components/timetable/messages/icons/CancelledTrip.svelte";
 
 	let { arrival, departure }: { arrival?: Time; departure?: Time } = $props();
+
+	const duration = () => calculateDuration(
+		DateTime.fromISO(departure?.actualTime ?? departure?.plannedTime ?? ""),
+		DateTime.fromISO(arrival?.actualTime ?? arrival?.plannedTime ?? ""),
+		["minutes"]
+	).as("minutes");
 </script>
 
-<div class="flex min-h-32 flex-row text-base">
-	<span class="text-text/65 basis-1/6 self-center text-right">{formatDuration(departure, arrival)}</span>
-	<div class="relative flex basis-1/6 md:max-w-[5%] justify-center">
+<div class="relative flex flex-row text-base py-12">
+	<span class="text-text/65 basis-1/6 self-center text-right">
+		{duration() > 0 ? formatDuration(departure, arrival) : ""}
+	</span>
+	<div class="flex basis-1/6 md:max-w-[5%] justify-center">
 		<span
 			class="absolute inset-y-4 z-0 h-full w-[4px] self-center bg-[repeating-linear-gradient(0deg,_#9ca3af_0px,_#9ca3af_2px,_transparent_0px,_transparent_5px)]"
 		></span>
 	</div>
-	<span class="flex basis-4/6 items-center self-center">
-		<Walking height="35px" width="35px" class="stroke-accent" />
-		Changeover
-	</span>
+	<div class="flex flex-col basis-4/6 justify-center gap-y-2">
+		<span class="flex items-center">
+			<Walking height="35px" width="35px" class="stroke-accent" />
+			Changeover
+		</span>
+		{#if duration() < 0}
+			<div class="text-background bg-white p-1">
+				<div class="flex flex-row gap-x-2">
+					<CancelledTrip />
+					<span class="font-bold">Warning:</span>
+				</div>
+				<span>Changeover probably not possible</span>
+			</div>
+		{/if}
+	</div>
 </div>
