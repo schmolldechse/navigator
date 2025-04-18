181d08b3b93433e8ecabbc56ea27716bc5fe577b
Christian Knapp <christian.alex.knapp@icloud.com>
2025-04-18 19:18:33 +0200

    feat(RoutePlanner, #97): add warning when there are no routes available

---
+++ frontend/src/components/route-planner/WarningNoRoutes.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import TriangleAlert from "lucide-svelte/icons/triangle-alert";
+</script>
+
+<div class="flex gap-x-4 items-center px-5">
+	<TriangleAlert size="45px" class="stroke-accent/85 stroke-2 shrink-0" />
+	<div class="flex flex-col">
+		<span class="text-xl font-bold tracking-tighter">No Routes Available</span>
+		<span class="text-md">There are currently no routes available. Please try to look for an earlier/ later time.</span>
+	</div>
+</div>
