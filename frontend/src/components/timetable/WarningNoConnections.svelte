df1ff1eeabf916c70879e9bf174c012a51a1d667
Christian Knapp <christian.alex.knapp@icloud.com>
2025-04-18 19:02:20 +0200

    feat(Timetable, #97): add warning when there's no connection available

---
+++ frontend/src/components/timetable/WarningNoConnections.svelte
@@ -0,0 +1,11 @@
+<script>
+	import TriangleAlert from "lucide-svelte/icons/triangle-alert";
+</script>
+
+<div class="flex gap-x-4 items-center px-5">
+	<TriangleAlert size="45px" class="stroke-accent/85 stroke-2 shrink-0" />
+	<div class="flex flex-col">
+		<span class="text-xl font-bold tracking-tighter">No Connections Available</span>
+		<span class="text-md">There are currently no connections available. Please check your applied filters or try again at a different time.</span>
+	</div>
+</div>
