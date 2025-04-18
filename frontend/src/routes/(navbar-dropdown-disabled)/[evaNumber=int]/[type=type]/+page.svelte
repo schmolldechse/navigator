f02aedfd35b643b4e0a4e7488776eda52aee3681
Christian Knapp <christian.alex.knapp@icloud.com>
2025-04-18 19:01:30 +0200

    refactor(Timetable): simplified expressions

--- frontend/src/routes/(navbar-dropdown-disabled)/[evaNumber=int]/[type=type]/+page.svelte
+++ frontend/src/routes/(navbar-dropdown-disabled)/[evaNumber=int]/[type=type]/+page.svelte
@@ -1,49 +1,45 @@
 <script lang="ts">
 	import type { PageProps } from "./$types";
 	import Filter from "$components/timetable/filter/Filter.svelte";
-	import type { Connection, Journey } from "$models/connection";
+	import type { Journey } from "$models/connection";
 	import ConnectionComponent from "$components/timetable/ConnectionComponent.svelte";
 	import WingTrain from "$components/timetable/WingTrain.svelte";
 	import Loader from "$components/Loader.svelte";
 
 	let { data }: PageProps = $props();
 
-	let currentFilter = $state(["*"]);
-
+	let currentFilter = $state<string[]>(["*"]);
 	const matchesFilter = (journey: Journey) => {
-		if (currentFilter.length === 0) return true;
-		if (currentFilter.includes("*")) return true;
-
-		const firstConnection: Connection = journey?.connections[0];
-		if (firstConnection === undefined) return false;
-
-		return currentFilter.includes(firstConnection?.lineInformation?.type ?? "");
+		if (currentFilter.length === 0 || currentFilter.includes("*")) return true;
+		return journey?.connections?.some(connection => currentFilter.includes(connection?.lineInformation?.type ?? "UNKNOWN"));
 	};
 
-	const allowedProducts = (journeys: Journey[] | undefined): string[] => {
-		const products = data.station?.products || [];
-
-		const types = journeys!
-			.flatMap((journey) => journey.connections.map((conn) => conn.lineInformation?.type))
-			.filter(Boolean);
-
-		return products.filter((product) => types.includes(product));
+	// returns all products that are used in the Connections
+	const allowedProducts = (journeys: Journey[]): string[] => {
+		const availableProducts = data.station?.products || [];
+		const connectionProducts = journeys.flatMap(journey => journey.connections
+			.flatMap(connection => connection.lineInformation?.type)
+			.filter(Boolean)
+		);
+		return availableProducts.filter((product: string) => connectionProducts.includes(product));
 	};
 </script>
 
+<!-- TODO: make Connection Skeleton -->
 {#await data.journeys}
 	<div class="mx-auto flex-1">
 		<Loader />
 	</div>
 {:then journeys}
-	<div class="scrollbar-track-sky-300 container mx-auto flex flex-1 flex-col divide-y-2 md:divide-y-0">
 		{#each journeys as journey}
 			{#if !matchesFilter(journey)}{:else if journey.connections.length > 1}
 				<WingTrain {journey} />
 			{:else}
-				<ConnectionComponent connection={journey.connections[0]} renderInformation={true} renderBorder={true} />
 			{/if}
 		{/each}
+	<div class="container mx-auto flex flex-1 flex-col pt-2 divide-y-2 md:divide-y-0">
+					<ConnectionComponent connection={journey.connections[0]} renderInformation={true}
+										 renderBorder={true} />
 	</div>
 
 	<div class="sticky bottom-0 mt-auto w-full">
