d031c423048d8dec27960b1aa0cc290fb5f9358a
Christian Knapp <christian.alex.knapp@icloud.com>
2025-04-22 18:48:09 +0200

    feat(RoutePlanner): added DateHeader component
    - this ensures visual clarity for the user as he knows which routes are planned for which date
    
    (cherry picked from commit 8408915877e12bd46fbbde4ba94104e001a2c8da)

---
+++ frontend/src/components/ui/info/DateHeader.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type {DateTime} from "luxon";
+
+    let { date }: { date: DateTime } = $props();
+</script>
+
+<div class="px-8 mb-4 mt-8">
+    <span class="block w-full text-lg md:text-xl border-b border-text/50 text-text">{date.toLocaleString({ weekday: "short", day: "numeric", month: "short", year: "numeric" })}</span>
+</div>
