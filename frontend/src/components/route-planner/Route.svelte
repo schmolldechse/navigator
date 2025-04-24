8d06f348d1a4e17dd166fb150bb55f19b19f1651
Christian Knapp <christian.alex.knapp@icloud.com>
2025-04-24 17:35:00 +0200

    feat(Route): add highlighting when route is not possible

--- frontend/src/components/route-planner/Route.svelte
+++ frontend/src/components/route-planner/Route.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import type { Route } from "$models/route";
-	import Minus from "lucide-svelte/icons/minus";
 	import TimeInformation from "$components/ui/info/TimeInformation.svelte";
 	import { calculateDuration, durationOfConnection, formatDuration } from "$lib";
 	import { DateTime } from "luxon";
-	import type { LineColor } from "$models/connection";
+	import type { Connection, LineColor } from "$models/connection";
 	import { env } from "$env/dynamic/public";
 	import ChevronDown from "lucide-svelte/icons/chevron-down";
 	import ChevronUp from "lucide-svelte/icons/chevron-up";
+	import Ban from "lucide-svelte/icons/ban";
+	import Minus from "lucide-svelte/icons/minus";
 	import LegInfo from "$components/route-planner/details/LegInfo.svelte";
 	import { onMount } from "svelte";
 	import Changeover from "$components/route-planner/details/Changeover.svelte";
@@ -31,6 +32,8 @@
 			)
 			.reduce((acc, el) => acc + el);
 	});
+
+	const isCancelled: boolean = $derived.by(() => route?.legs?.some((leg: Connection) => leg?.cancelled));
 
 	const getWidthRatio = (duration: number, maxDuration: number) => {
 		if (maxDuration <= 0) return "0%";
@@ -59,17 +62,28 @@
 	const normalize = (value: string): string => value.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "");
 </script>
 
-<div class="border-primary-dark/75 space-y-2 rounded-lg border-2 px-4 py-2 text-2xl font-medium">
+<div class="border-primary-dark/75 rounded-lg border-2 text-2xl font-medium overflow-hidden"
+	 class:opacity-65={isCancelled}
+>
+	<!-- Cancelled Header -->
+	{#if isCancelled}
+		<div class="bg-secondary/25 flex flex-row items-center gap-x-2 py-1 px-3">
+			<Ban size="20" />
+			<span class="text-sm md:text-lg font-semibold">Route not possible</span>
+		</div>
+	{/if}
+
+	<div class="px-4 py-2 space-y-2">
 	<!-- Time Info -->
 	<div class="flex flex-row items-baseline gap-x-2">
 		<div class="flex flex-row">
 			<TimeInformation time={route?.legs[0]?.departure} direction="col" class="text-xl" delayClass="text-sm md:text-lg" />
 			<Minus class="mx-2 mt-[0.15rem]" />
 			<TimeInformation
-				time={route?.legs[route?.legs?.length - 1]?.arrival}
-				direction="col"
-				class="text-xl"
-				delayClass="text-sm md:text-lg"
+					time={route?.legs[route?.legs?.length - 1]?.arrival}
+					direction="col"
+					class="text-xl"
+					delayClass="text-sm md:text-lg"
 			/>
 		</div>
 
@@ -95,8 +109,8 @@
 	<div class="accent-scrollbar flex w-full flex-row gap-x-2 overflow-x-auto">
 		{#each route?.legs.filter((leg) => !leg?.walking) as leg}
 			<span
-				class="bg-primary-darker line-clamp-1 min-w-fit truncate rounded-lg px-2 py-1 text-center text-base md:line-clamp-none md:text-lg"
-				style:width={getWidthRatio(durationOfConnection(leg), durationWithoutWalking())}
+					class="bg-primary-darker line-clamp-1 min-w-fit truncate rounded-lg px-2 py-1 text-center text-base md:line-clamp-none md:text-lg"
+					style:width={getWidthRatio(durationOfConnection(leg), durationWithoutWalking())}
 			>
 				{leg?.lineInformation?.lineName}
 			</span>
@@ -129,9 +143,9 @@
 						stopWalking is the time, at which the next leg starts (route?.legs[i + 1]?.departure)
 						 -->
 						<Changeover
-							startWalking={leg?.departure}
-							stopWalking={route?.legs[i + 1]?.departure}
-							firstIsWalking={leg?.origin}
+								startWalking={leg?.departure}
+								stopWalking={route?.legs[i + 1]?.departure}
+								firstIsWalking={leg?.origin}
 						/>
 					{:else if lastIsWalking}
 						<!--
@@ -148,8 +162,8 @@
 					{/if}
 				{:else}
 					<LegInfo
-						{leg}
-						lineColor={legColors.find(
+							{leg}
+							lineColor={legColors.find(
 							(color) => normalize(color.lineName) === normalize(leg?.lineInformation?.lineName ?? "")
 						)}
 					/>
@@ -157,4 +171,5 @@
 			{/each}
 		</div>
 	{/if}
+	</div>
 </div>
