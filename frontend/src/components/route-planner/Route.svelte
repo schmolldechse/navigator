24b3a975d8b657f1d1ee3851ecdc28746237cb24
Christian Knapp <christian.alex.knapp@icloud.com>
2025-04-29 22:45:34 +0200

    refactor(Route): improved styling and layout
    - improved background color, border and legs for better readability
    - set letter spacing for Route Header
    - removed padding in Route details

--- frontend/src/components/route-planner/Route.svelte
+++ frontend/src/components/route-planner/Route.svelte
@@ -39,42 +39,42 @@
 	const normalize = (value: string): string => value.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "");
 </script>
 
-<div class="border-primary-dark/75 overflow-hidden rounded-lg border-2 text-2xl font-medium">
+<div class="border-primary/45 overflow-hidden rounded-lg border-2 py-2">
 	<!-- Cancelled Header -->
 	{#if isCancelled}
-		<div class="bg-secondary/35 flex flex-row items-center gap-x-2 px-3 py-1">
+		<div class="bg-secondary/50 flex flex-row items-center gap-x-2 px-3 py-1">
 			<Ban size="20" />
 			<span class="text-sm font-semibold md:text-lg">Route not possible</span>
 		</div>
 	{/if}
 
-	<div class="space-y-2 px-4 py-2" class:opacity-65={isCancelled}>
+	<div class="px-4 space-y-2" class:opacity-65={isCancelled}>
 		<!-- Time Info -->
-		<div class="flex flex-row items-baseline gap-x-2">
-			<div class="flex flex-row">
+		<div class="flex flex-row items-baseline gap-x-2 tracking-tight">
+			<div class="flex flex-row gap-x-1">
 				<TimeInformation
 					time={route?.legs[0]?.departure}
 					direction="col"
-					class="text-xl"
-					delayClass="text-sm md:text-lg"
+					class="font-bold text-base md:text-xl"
+					delayClass="text-sm md:text-base"
 				/>
-				<Minus class="mx-2 mt-[0.15rem]" />
+				<Minus class="mt-[0.05rem] md:mt-[0.15rem]" />
 				<TimeInformation
 					time={route?.legs[route?.legs?.length - 1]?.arrival}
 					direction="col"
-					class="text-xl"
-					delayClass="text-sm md:text-lg"
+					class="font-bold text-base md:text-xl"
+					delayClass="text-sm md:text-base"
 				/>
 			</div>
 
-			<span class="text-primary/90">|</span>
-			<span class="mt-[0.35rem] text-sm md:text-lg">
+			<span class="text-text/50 font-medium">|</span>
+			<span class="text-sm">
 				{formatDuration(route?.legs[route?.legs?.length - 1]?.arrival, route?.legs[0]?.departure)}
 			</span>
 
 			{#if route?.legs?.filter((leg) => leg?.walking).length > 0}
-				<span class="text-primary/90">|</span>
-				<div class="mt-[0.35rem] flex items-baseline text-sm md:text-lg">
+				<span class="text-text/50 font-medium">|</span>
+				<div class="flex items-baseline text-sm">
 					<span>{route?.legs?.filter((leg) => leg?.walking).length}</span>
 
 					<div class="flex items-center">
@@ -89,7 +89,7 @@
 		<div class="accent-scrollbar flex w-full flex-row gap-x-2 overflow-x-auto">
 			{#each route?.legs.filter((leg) => !leg?.walking) as leg}
 				<span
-					class="bg-primary-darker line-clamp-1 min-w-fit truncate rounded-lg px-2 py-1 text-center text-base md:line-clamp-none md:text-lg"
+					class="bg-primary/25 min-w-fit truncate rounded-lg px-2 py-1 text-center text-base md:text-lg font-medium"
 					style:width={getWidthRatio(durationOfConnection(leg), durationWithoutWalking)}
 				>
 					{leg?.lineInformation?.lineName}
@@ -98,7 +98,8 @@
 		</div>
 
 		<div class="flex justify-center">
-			<button class="flex cursor-pointer flex-row items-center gap-x-2" onclick={() => (detailsOpen = !detailsOpen)}>
+			<button class="flex cursor-pointer flex-row items-center gap-x-2"
+					onclick={() => (detailsOpen = !detailsOpen)}>
 				<span class="text-sm md:text-lg">Details</span>
 				{#if !detailsOpen}
 					<ChevronDown color="#ffda0a" />
@@ -107,57 +108,57 @@
 				{/if}
 			</button>
 		</div>
+	</div>
 
-		{#if detailsOpen}
-			<div class="border-primary-dark/75 flex flex-col border-t">
-				<span class="text-lg font-semibold">Route Details</span>
+	{#if detailsOpen}
+		<div class="md:px-2 border-primary-dark/75 flex flex-col border-t">
+			<span class="text-lg font-semibold p-4 pb-8 tracking-tighter">Route Details</span>
 
-				{#each route?.legs as leg, i}
-					{#if leg?.walking}
-						{@const firstIsWalking = i === 0}
-						{@const lastIsWalking = i === route?.legs.length - 1}
+			{#each route?.legs as leg, i}
+				{#if leg?.walking}
+					{@const firstIsWalking = i === 0}
+					{@const lastIsWalking = i === route?.legs.length - 1}
 
-						{#if firstIsWalking}
-							<!--
+					{#if firstIsWalking}
+						<!--
 						startWalking is the time, at which you set off from the stop (leg?.departure)
 						stopWalking is the time, at which the next leg starts (route?.legs[i + 1]?.departure)
-						 -->
-							<Changeover
-								startWalking={leg?.departure}
-								stopWalking={route?.legs[i + 1]?.departure}
-								firstIsWalking={leg?.origin}
-							/>
-						{:else if lastIsWalking}
-							<!--
+						-->
+						<Changeover
+							startWalking={leg?.departure}
+							stopWalking={route?.legs[i + 1]?.departure}
+							firstIsWalking={leg?.origin}
+						/>
+					{:else if lastIsWalking}
+						<!--
 						startWalking is the time, at which you set off from the stop (leg?.departure)
 						stopWalking is the time, at which you arrive at the destination (leg?.arrival)
-						 -->
-							<Changeover
-								startWalking={leg?.departure}
-								stopWalking={leg?.arrival}
-								lastIsWalking={leg?.destination}
-							/>
-						{:else}
-							<!--
+						-->
+						<Changeover
+							startWalking={leg?.departure}
+							stopWalking={leg?.arrival}
+							lastIsWalking={leg?.destination}
+						/>
+					{:else}
+						<!--
 						startWalking is the time at which you arrive at the last stop (route?.legs[i - 1]?.arrival)
 						stopWalking is the time at which you depart at the next stop (route?.legs[i + 1]?.departure)
-						 -->
-							<Changeover
-								startWalking={route?.legs[i - 1]?.arrival}
-								stopWalking={route?.legs[i + 1]?.departure}
-							/>
-						{/if}
-					{:else}
-						<LegInfo
-							{leg}
-							lineColor={lineColors.find(
+						-->
+						<Changeover
+							startWalking={route?.legs[i - 1]?.arrival}
+							stopWalking={route?.legs[i + 1]?.departure}
+						/>
+					{/if}
+				{:else}
+					<LegInfo
+						{leg}
+						lineColor={lineColors.find(
 								(color: LineColor) =>
 									normalize(color.lineName) === normalize(leg?.lineInformation?.lineName ?? "")
 							)}
-						/>
-					{/if}
-				{/each}
-			</div>
-		{/if}
-	</div>
+					/>
+				{/if}
+			{/each}
+		</div>
+	{/if}
 </div>
