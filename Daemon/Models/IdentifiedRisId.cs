da22cfeb1240b5d5c9829111a0217ccadcd94522
Christian Knapp <christian.alex.knapp@icloud.com>
2025-04-30 17:01:47 +0200

    refactor(daemon): extracted JSON parsing to `IdentifiedRisId`
    (cherry picked from commit 6fde703451cf16e686d7eca797ee57715009a75d)

--- Daemon/Models/IdentifiedRisId.cs
+++ Daemon/Models/IdentifiedRisId.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json.Linq;
 
 namespace Daemon.Models;
 
@@ -9,7 +11,7 @@ public class IdentifiedRisId
     [Required]
     [BsonId]
     [BsonElement("risId")]
-    public string risId { get; set; }
+    public required string RisId { get; set; }
 
     [BsonElement("discoveredAt")]
     [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
@@ -18,4 +20,29 @@ public class IdentifiedRisId
     [BsonElement("lastSuccessfulQueried")]
     [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
     public DateTime? LastSuccessfulQueried { get; set; }
+
+    public static IdentifiedRisId FromJson(JToken jsonObject)
+    {
+        if (jsonObject == null)
+            throw new ArgumentNullException(nameof(jsonObject), "JSON object for IdentifiedRisId cannot be null");
+        
+        string fullTripId = jsonObject["tripId"]?.ToString() ??  throw new ArgumentException("Missing 'tripId' field in IdentifiedRisId");
+        string datePart = fullTripId.Substring(0, 8);
+
+        if (DateTime.TryParseExact(datePart, "yyyyMMdd", null, DateTimeStyles.None, out DateTime _))
+            fullTripId = fullTripId.Substring(9);
+
+        DateTime? discoveredAt = null;
+        if (jsonObject["plannedWhen"] != null)
+            discoveredAt = new DateTime(
+                DateOnly.FromDateTime(DateTime.Parse(jsonObject["plannedWhen"]!.ToString()).Date),
+                TimeOnly.FromTimeSpan(DateTime.Now.Date.TimeOfDay)
+            ).ToLocalTime();
+        
+        return new IdentifiedRisId()
+        {
+            RisId = fullTripId,
+            DiscoveredAt = discoveredAt
+        };
+    }
 }
