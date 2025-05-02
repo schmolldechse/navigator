fbaff9cb0c93f1fe4c2823efb84e38a08c8d1212
Christian Knapp <christian.alex.knapp@icloud.com>
2025-05-02 20:14:24 +0200

    refactor(DateHeader): simplified margin

--- frontend/src/components/ui/info/DateHeader.svelte
+++ frontend/src/components/ui/info/DateHeader.svelte
@@ -4,7 +4,7 @@
 	let { date }: { date: DateTime } = $props();
 </script>
 
-<div class="my-4 mx-4 lg:mx-8">
+<div class="m-4 lg:mx-8">
 	<span class="border-text/50 text-text block w-full border-b text-lg md:text-xl">
 		{date.toLocaleString({ weekday: "short", day: "numeric", month: "short", year: "numeric" })}
 	</span>
