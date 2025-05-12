7edc6ae15a022681703f2b86e9946664973146fe
Christian Knapp <christian.alex.knapp@icloud.com>
2025-05-12 16:43:24 +0200

    WIP feat(backend): added POST method
    - this endpoint is used later for the statistics
    - added InDto

--- backend/src/controllers/station.controller.ts
+++ backend/src/controllers/station.controller.ts
@@ -3,8 +3,11 @@ import { StationService } from "../services/station.service";
 import { HttpStatus } from "../response/status";
 import { type Station } from "navigator-core/src/models/station";
 import { HttpError } from "../response/error";
+import { DateTime } from "luxon";
 
 const service = new StationService();
+
+const START_DATE = DateTime.fromObject({ day: 17, month: 12, year: 2024 }).startOf("day");
 
 const stationController = new Elysia({ prefix: "/station", tags: ["Stations"] })
 	.decorate({ httpStatus: HttpStatus })
@@ -41,6 +44,25 @@ const stationController = new Elysia({ prefix: "/station", tags: ["Stations"] })
 					"Get a station by its evaNumber. Returns the station object if available or fetches and caches it if not found locally."
 			}
 		}
-	);
-
+	)
+	.post("/stats/:evaNumber", ({ params: { evaNumber } }) => {}, {
+		body: t.Object({
+			startDate: t.Date({ default: START_DATE.toFormat("yyyy-MM-dd"), description: "Start date of the filter" }),
+			endDate: t.Date({ default: DateTime.now().toFormat("yyyy-MM-dd"), description: "End date of the filter" }),
+			filter: t.Object({
+				delayThreshold: t.Number({
+					default: 60,
+					description: "Defines the threshold in seconds after which a delay is considered",
+					minimum: 0,
+					maximum: Number.MAX_SAFE_INTEGER
+				}),
+				products: t.Array(t.String(), { default: [], description: "The products to look for" }),
+				lineName: t.Array(t.String(), { default: [], description: "The lineName to look for, e.g. ´MEX 12´" }),
+				lineNumber: t.Array(t.String(), {
+					default: [],
+					description: "The exact lineNumber to look for, e.g. ´19974´"
+				})
+			})
+		})
+	});
 export default stationController;
