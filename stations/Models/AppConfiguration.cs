5e80c2048aa2c52bfd24d325474d21d256fbd385
Christian Knapp <christian.alex.knapp@icloud.com>
2025-05-24 20:39:21 +0200

    feat(core): new `stations` executable program

---
+++ stations/Models/AppConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace stations.Models;
+
+public record AppConfiguration
+{
+    public string ClientId { get; init; } = string.Empty;
+    public string ClientSecret { get; init; } = string.Empty;
+};
