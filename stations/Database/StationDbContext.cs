06b17f487b4ce32a507ed616ea8d79a6cc545772
Christian Knapp <christian.alex.knapp@icloud.com>
2025-05-25 11:19:41 +0200

    feat(stations): add database migrations using EF

---
+++ stations/Database/StationDbContext.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
+using stations.Models.Database;
+
+namespace stations.Database;
+
+public class StationDbContext : DbContext
+{
+    public DbSet<Station> Stations { get; set; }
+
+    private readonly Regex _uriRegex =
+        new(@"^postgresql://(?:([^:]+)(?::([^@]+))?@)?([^:/]+)(?::(\d+))?(?:/([^?]+))?(?:\?(.*))?$");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("data");
+        modelBuilder.Entity<Station>(entity =>
+        {
+            entity.ToTable("stations");
+            entity.HasKey(e => e.EvaNumber);
+            entity.Property(e => e.Ril100).HasColumnType("text[]");
+            entity.Property(e => e.Products).HasColumnType("text[]");
+            
+            // map back Latitude and Longitude to Coordinates
+            entity.Property<double>("_latitude").HasColumnName("Latitude").IsRequired();
+            entity.Property<double>("_longitude").HasColumnName("Longitude").IsRequired();
+        });
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("POSTGRES_DATABASE") ?? throw new ArgumentNullException("POSTGRES_DATABASE", "PostgreSQL connection string not configured");
+        
+        var parsed = ParseToEfConnectionString(connectionString);
+        optionsBuilder.UseNpgsql(parsed);
+    }
+
+    private string ParseToEfConnectionString(string connectionString)
+    {
+        var match = _uriRegex.Match(connectionString);
+        if (!match.Success) throw new FormatException("Invalid PostgreSQL connection string");
+        
+        var user = match.Groups[1].Value;
+        var password = match.Groups[2].Value;
+        var host = match.Groups[3].Value;
+        var port = match.Groups[4].Value ?? "5432"; // Default PostgreSQL port
+        var database = match.Groups[5].Value;
+
+        var builder = new StringBuilder();
+        builder.Append($"Host={host};");
+        if (int.TryParse(port, out _)) builder.Append($"Port={port};");
+        if (!string.IsNullOrEmpty(user)) builder.Append($"Username={user};");
+        if (!string.IsNullOrEmpty(password)) builder.Append($"Password={password};");
+        if (!string.IsNullOrEmpty(database)) builder.Append($"Database={database};");
+        
+        // parse any additional parameters
+        if (match.Groups[6].Success)
+        {
+            var query = match.Groups[6].Value;
+            var queryParams = System.Web.HttpUtility.ParseQueryString(query);
+            foreach (string key in queryParams)
+            {
+                if (string.IsNullOrEmpty(key)) continue;
+                builder.Append($"{key}={queryParams[key]};");
+            }
+        }
+        return builder.ToString().TrimEnd(';');
+    }
+}
---
+++ stations/Migrations/20250525091740_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace stations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "data");
+
+            migrationBuilder.CreateTable(
+                name: "stations",
+                schema: "data",
+                columns: table => new
+                {
+                    EvaNumber = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    Ril100 = table.Column<List<string>>(type: "text[]", nullable: true),
+                    Products = table.Column<List<string>>(type: "text[]", nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", nullable: false),
+                    Longitude = table.Column<double>(type: "double precision", nullable: false),
+                    QueryingEnabled = table.Column<bool>(type: "boolean", nullable: true),
+                    LastQueried = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_stations", x => x.EvaNumber);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "stations",
+                schema: "data");
+        }
+    }
+}
