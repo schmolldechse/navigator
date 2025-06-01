7b936847bbd3725282390d02d77d2dd884d40c2d
Christian Knapp <christian.alex.knapp@icloud.com>
2025-06-01 22:40:43 +0200

    WIP feat(backend): add core database schema to match with daemon

---
+++ backend/src/db/core.schema.ts
@@ -0,0 +1,37 @@
+import { pgSchema, uuid, varchar, timestamp, integer, boolean } from "drizzle-orm/pg-core";
+
+const coreSchema = pgSchema("core");
+
+// ris_ids
+const risIds = coreSchema.table("ris_ids", {
+	id: uuid("id").notNull().primaryKey(),
+	product: varchar("product", { length: 128 }).notNull(),
+	discoveryDate: timestamp("discovery_date", { mode: "date" }).notNull(),
+	lastSeenDate: timestamp("last_seen", { mode: "date" }),
+	lastSucceededAt: timestamp("last_succeeded_at", { mode: "date" }),
+});
+
+// stations
+const stations = coreSchema.table("stations", {
+	evaNumber: integer("eva_number").notNull().primaryKey(),
+	name: varchar("name", { length: 512 }).notNull(),
+	latitude: integer("latitude"),
+	longitude: integer("longitude"),
+	queryingEnabled: boolean("querying_enabled"),
+	lastQueried: timestamp("last_queried", { mode: "date" }),
+});
+
+const stationProducts = coreSchema.table("station_products", {
+	id: integer("id").notNull().primaryKey(),
+	evaNumber: integer("eva_number").notNull().references(() => stations.evaNumber, { onDelete: "cascade" }),
+	name: varchar("name", { length: 32 }).notNull(),
+	queryingEnabled: boolean("querying_enabled").notNull(),
+});
+
+const stationRil = coreSchema.table("station_ril100", {
+	id: integer("id").notNull().primaryKey(),
+	evaNumber: integer("eva_number").notNull().references(() => stations.evaNumber, { onDelete: "cascade" }),
+	ril100: varchar("ril100", { length: 32 }).notNull(),
+});
+
+export { risIds, stations, stationProducts, stationRil };
