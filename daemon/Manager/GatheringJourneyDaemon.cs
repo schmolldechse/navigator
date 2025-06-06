6b0b7c7c08855d4db011f9d8c3392a1d0d70994e
Christian Knapp <christian.alex.knapp@icloud.com>
2025-06-06 21:49:05 +0200

    fix(daemon): wrong parsing of messages

--- daemon/Manager/GatheringJourneyDaemon.cs
+++ daemon/Manager/GatheringJourneyDaemon.cs
@@ -17,7 +17,8 @@ public class GatheringJourneyDaemon : Daemon
 
     private readonly string _apiUrl = "https://regio-guide.de/@prd/zupo-travel-information/api/public/ri/journey/{0}";
 
-    public GatheringJourneyDaemon(ILogger<GatheringJourneyDaemon> logger, IServiceProvider serviceProvider, ProxyRotator proxyRotator)
+    public GatheringJourneyDaemon(ILogger<GatheringJourneyDaemon> logger, IServiceProvider serviceProvider,
+        ProxyRotator proxyRotator)
         : base("Gather Journey", TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(300), logger)
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger), "Logger cannot be null");
@@ -76,7 +77,8 @@ public class GatheringJourneyDaemon : Daemon
         }
     }
 
-    private async Task ProcessJourney(IdentifiedRisId risId, DateTime date, NavigatorDbContext dbContext, CancellationToken cancellationToken)
+    private async Task ProcessJourney(IdentifiedRisId risId, DateTime date, NavigatorDbContext dbContext,
+        CancellationToken cancellationToken)
     {
         JourneyResponse journeyResponse = await CallApi(risId.Id.ToString(), date, cancellationToken);
         if (journeyResponse.ParsingError)
@@ -84,26 +86,27 @@ public class GatheringJourneyDaemon : Daemon
             _logger.LogError("Failed to parse journey for RIS ID {RisId}", risId.Id);
             return;
         }
-        
+
         // first update risId
         risId.LastSeen = date;
 
         if (journeyResponse.Journey != null)
         {
             risId.LastSucceededAt = date;
-            
+
             // check before adding
             var exists = await dbContext.Journeys.AnyAsync(j => j.Id == journeyResponse.Journey.Id, cancellationToken);
             if (!exists) dbContext.Journeys.Add(journeyResponse.Journey);
         }
-        
+
         await dbContext.SaveChangesAsync(cancellationToken);
     }
 
     private async Task<JourneyResponse> CallApi(string risId, DateTime when, CancellationToken cancellationToken)
     {
         string formattedId = when.ToString("yyyyMMdd") + "-" + risId;
-        
+        _logger.LogDebug("Gathering journey for RIS id {0}", formattedId);
+
         // random proxy
         using var httpClient = _proxyRotator.GetRandomProxy();
         var request = await httpClient.GetAsync(string.Format(_apiUrl, formattedId), cancellationToken);
@@ -144,7 +147,13 @@ public class GatheringJourneyDaemon : Daemon
             },
             Messages = content.GetProperty("hims").EnumerateArray().Select(message => new JourneyMessage()
             {
-                Code = int.Parse(message.GetProperty("code").GetString() ?? throw new ArgumentNullException("Message code cannot be null")),
+                Code = int.Parse(
+                    message.TryGetProperty("code", out var codeProp) && codeProp.ValueKind != JsonValueKind.Null &&
+                    codeProp.GetString() != null ? codeProp.GetString() :
+                    message.TryGetProperty("id", out var idProp) && idProp.ValueKind != JsonValueKind.Null &&
+                    idProp.GetString() != null ? idProp.GetString() :
+                    throw new ArgumentNullException("Message code or id cannot be null")
+                ),
                 Message = message.GetProperty("caption").GetString() ?? string.Empty,
                 Summary = message.GetProperty("shortText").GetString() ?? string.Empty
             }).ToList(),
@@ -155,23 +164,25 @@ public class GatheringJourneyDaemon : Daemon
                 var stopObj = new Stop()
                 {
                     Cancelled = cancelled,
-                    EvaNumber = int.Parse(stop.GetProperty("station").GetProperty("evaNo").GetString() ?? throw new ArgumentNullException("evaNo cannot be null")),
+                    EvaNumber = int.Parse(stop.GetProperty("station").GetProperty("evaNo").GetString() ??
+                                          throw new ArgumentNullException("evaNo cannot be null")),
                     Name = stop.GetProperty("station").GetProperty("name").GetString() ??
                            throw new ArgumentNullException("Station name cannot be null"),
                     Messages = stop.GetProperty("messages").EnumerateArray().Select(message => new StopMessage()
-                        {
-                            Code = int.Parse(message.GetProperty("code").GetString() ??
-                                             throw new ArgumentNullException("Message code cannot be null")),
-                            Message = message.GetProperty("text").GetString() ?? string.Empty,
-                            Summary = message.TryGetProperty("textShort", out var textShortProp) &&
-                                      textShortProp.ValueKind != JsonValueKind.Null &&
-                                      textShortProp.GetString() != null
-                                ? textShortProp.GetString()!
-                                : message.GetProperty("text").GetString()!
+                    {
+                        Code = int.Parse(message.GetProperty("code").GetString() ??
+                                         throw new ArgumentNullException("Message code cannot be null")),
+                        Message = message.GetProperty("text").GetString() ?? string.Empty,
+                        Summary = message.TryGetProperty("textShort", out var textShortProp) &&
+                                  textShortProp.ValueKind != JsonValueKind.Null &&
+                                  textShortProp.GetString() != null
+                            ? textShortProp.GetString()!
+                            : message.GetProperty("text").GetString()!
                     }).ToList()
                 };
 
-                if (stop.TryGetProperty("arrivalTime", out var arrivalTimeProp) && arrivalTimeProp.ValueKind != JsonValueKind.Null)
+                if (stop.TryGetProperty("arrivalTime", out var arrivalTimeProp) &&
+                    arrivalTimeProp.ValueKind != JsonValueKind.Null)
                 {
                     var actualTime = DateTime.Parse(
                         arrivalTimeProp.GetProperty("predicted").GetString() ?? string.Empty,
@@ -188,13 +199,15 @@ public class GatheringJourneyDaemon : Daemon
                         ActualTime = actualTime,
                         PlannedTime = plannedTime,
                         Delay = (int)(actualTime - plannedTime).TotalSeconds,
-                        ActualPlatform = stop.GetProperty("track").GetProperty("prediction").GetString() ?? string.Empty,
+                        ActualPlatform =
+                            stop.GetProperty("track").GetProperty("prediction").GetString() ?? string.Empty,
                         PlannedPlatform = stop.GetProperty("track").GetProperty("target").GetString() ?? string.Empty
                     };
                     stopObj.Arrival = arrivalTime;
                 }
 
-                if (stop.TryGetProperty("departureTime", out var departureTimeProp) && departureTimeProp.ValueKind != JsonValueKind.Null)
+                if (stop.TryGetProperty("departureTime", out var departureTimeProp) &&
+                    departureTimeProp.ValueKind != JsonValueKind.Null)
                 {
                     var actualTime = DateTime.Parse(
                         departureTimeProp.GetProperty("predicted").GetString() ?? string.Empty,
@@ -211,11 +224,13 @@ public class GatheringJourneyDaemon : Daemon
                         ActualTime = actualTime,
                         PlannedTime = plannedTime,
                         Delay = (int)(actualTime - plannedTime).TotalSeconds,
-                        ActualPlatform = stop.GetProperty("track").GetProperty("prediction").GetString() ?? string.Empty,
+                        ActualPlatform =
+                            stop.GetProperty("track").GetProperty("prediction").GetString() ?? string.Empty,
                         PlannedPlatform = stop.GetProperty("track").GetProperty("target").GetString() ?? string.Empty
                     };
                     stopObj.Departure = departureTime;
                 }
+
                 return stopObj;
             }).ToList()
         };
