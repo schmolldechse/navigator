74648f76796cfb1f2256ee9837a67756eaa98ef7
Christian Knapp <christian.alex.knapp@icloud.com>
2025-06-12 20:15:17 +0200

    refactor(backend): extracted methods into a separate helper

--- backend/src/services/timetable.service.ts
+++ backend/src/services/timetable.service.ts
@@ -1,13 +1,12 @@
 import { t } from "elysia";
 import { DateTimeObject } from "../types/datetime";
 import { DateTime } from "luxon";
-import { Message, Time, TimetableEntry, TimetableStop } from "../models/core/models";
+import { TimetableEntry } from "../models/core/models";
 import { SingleTimetableEntrySchema } from "../models/elysia/timetable.model";
-import { mapToProduct } from "../models/core/products";
-import { extractJourneyNumber, extractLeadingLetters, normalize } from "../lib/regex";
 import { database } from "../db/postgres";
 import { stations } from "../db/core.schema";
-import { eq, inArray } from "drizzle-orm";
+import { inArray } from "drizzle-orm";
+import { TimetableHelper } from "./timetable.helper";
 
 enum RequestType {
 	DEPARTURES = "departures",
@@ -42,6 +41,7 @@ class TimetableService {
 			error: "Parameter 'language' must be either 'de' or 'en'."
 		})
 	});
+	private readonly helper: TimetableHelper = new TimetableHelper();
 
 	retrieveCombined = async (
 		evaNumber: number,
@@ -57,7 +57,7 @@ class TimetableService {
 				this.retrieveRISConnections(evaNumber, type, queryParams),
 				this.retrieveHafasConnections(evaNumber, type, queryParams)
 			]);
-			return this.mergeTimetables(ris, hafas, type);
+			return this.helper.mergeTimetables(ris, hafas, type);
 		}
 
 		const remainingDuration: number = Math.ceil(endTime.diff(now, ["minute"]).minutes);
@@ -68,7 +68,7 @@ class TimetableService {
 				this.retrieveRISConnections(evaNumber, type, queryParams),
 				this.retrieveHafasConnections(evaNumber, type, queryParams)
 			]);
-			return this.mergeTimetables(ris, hafas, type);
+			return this.helper.mergeTimetables(ris, hafas, type);
 		}
 
 		// use Bahnhof, RIS & HAFAS
@@ -78,7 +78,7 @@ class TimetableService {
 			this.retrieveHafasConnections(evaNumber, type, queryParams)
 		]);
 
-		let mergedTimetables: TimetableEntry[] = this.mergeTimetables(bahnhof, this.mergeTimetables(ris, hafas, type), type);
+		let mergedTimetables: TimetableEntry[] = this.helper.mergeTimetables(bahnhof, this.helper.mergeTimetables(ris, hafas, type), type);
 		// remove entries before & after the requested time
 		mergedTimetables = mergedTimetables.filter((entry) => {
 			const firstEntryTime = DateTime.fromISO(entry.entries[0].timeInformation.plannedTime);
@@ -93,102 +93,6 @@ class TimetableService {
 			return aTime < bTime ? -1 : 1;
 		});
 		return mergedTimetables;
-	};
-
-	private mergeEntry = (
-		entryA: typeof SingleTimetableEntrySchema.static,
-		entryB: typeof SingleTimetableEntrySchema.static
-	): typeof SingleTimetableEntrySchema.static => ({
-		...entryB,
-		...entryA,
-		ris_journeyId: entryA.ris_journeyId ?? entryB.ris_journeyId,
-		hafas_journeyId: entryA.hafas_journeyId ?? entryB.hafas_journeyId,
-		origin: entryA.origin ?? entryB.origin,
-		destination: entryA.destination ?? entryB.destination,
-		timeInformation: entryA.timeInformation ?? entryB.timeInformation,
-		lineInformation: {
-			...entryB.lineInformation,
-			...entryA.lineInformation,
-			// prefer from entryB, as the RIS/ HAFAS entries are more complete
-			journeyNumber: entryA?.lineInformation.journeyNumber ?? entryB.lineInformation.journeyNumber,
-			operator: entryA?.lineInformation.operator ?? entryB.lineInformation.operator,
-		},
-		viaStops: entryA.viaStops && entryA.viaStops.length > 1 ? entryA.viaStops : entryB.viaStops,
-		messages: entryA.messages ? [...(entryA.messages ?? []), ...(entryB.messages ?? [])] : entryB.messages
-	});
-
-	// merge both timetables by preferring the entries from timetableA
-	private mergeTimetables = (
-		timetableEntriesA: TimetableEntry[],
-		timetableEntriesB: TimetableEntry[],
-		type: RequestType = RequestType.DEPARTURES
-	): TimetableEntry[] => {
-		if (timetableEntriesA.length === 0) return timetableEntriesB;
-		if (timetableEntriesB.length === 0) return timetableEntriesA;
-
-		// simplify timetableB as the entries are nested
-		let simplifiedTimetableEntriesB: (typeof SingleTimetableEntrySchema.static)[] = timetableEntriesB.flatMap(
-			(entry) => entry.entries
-		);
-		timetableEntriesA.forEach((timetableA: TimetableEntry) => {
-			timetableA.entries.map((singleTimetableA: typeof SingleTimetableEntrySchema.static) => {
-				// search for a matching entry in simplifiedTimetableEntriesB
-				const matchingEntry = simplifiedTimetableEntriesB.find(
-					(singleTimetableB: typeof SingleTimetableEntrySchema.static) =>
-						this.doesTimetableMatch(singleTimetableA, singleTimetableB, type)
-				);
-				if (matchingEntry) {
-					// remove singleTimetableB from simplifiedTimetableEntriesB
-					simplifiedTimetableEntriesB = simplifiedTimetableEntriesB.filter((entry) => entry !== matchingEntry);
-
-					// merge the two entries
-					Object.assign(singleTimetableA, this.mergeEntry(singleTimetableA, matchingEntry));
-					return singleTimetableA;
-				}
-
-				// keep original entry from timetableA
-				return singleTimetableA;
-			});
-		});
-
-		// insert remaining entries from simplifiedTimetableEntriesB
-		simplifiedTimetableEntriesB.forEach((singleTimetableB: typeof SingleTimetableEntrySchema.static) =>
-			timetableEntriesA.push({ entries: [singleTimetableB] })
-		);
-
-		return timetableEntriesA;
-	};
-
-	private doesTimetableMatch = (
-		timetableA: typeof SingleTimetableEntrySchema.static,
-		timetableB: typeof SingleTimetableEntrySchema.static,
-		type: RequestType = RequestType.DEPARTURES
-	): boolean => {
-		if (timetableA.ris_journeyId === timetableB.ris_journeyId) return true;
-		if (timetableA.hafas_journeyId === timetableB.hafas_journeyId) return true;
-
-		const matchesLine = (): boolean =>
-			normalize(timetableA.lineInformation.journeyName) === normalize(timetableB.lineInformation.journeyName) ||
-			timetableA.lineInformation.journeyNumber === timetableB.lineInformation.journeyNumber;
-
-		const matchesTime = (): boolean => {
-			const aTime = DateTime.fromISO(timetableA.timeInformation.plannedTime);
-			const bTime = DateTime.fromISO(timetableB.timeInformation.plannedTime);
-			if (!aTime.isValid || !bTime.isValid) return false;
-			return aTime.equals(bTime);
-		};
-
-		const matchesPlatform = (): boolean => {
-			if (!timetableA.timeInformation.plannedPlatform || !timetableB.timeInformation.plannedPlatform) return true;
-			return timetableA.timeInformation.plannedPlatform === timetableB.timeInformation.plannedPlatform;
-		};
-
-		const matchesStop = (): boolean => {
-			if (type === RequestType.DEPARTURES) return timetableA.origin.evaNumber === timetableB.origin.evaNumber;
-			return timetableA.destination.evaNumber === timetableB.destination.evaNumber;
-		};
-
-		return matchesLine() && matchesTime() && matchesPlatform() && matchesStop();
 	};
 
 	retrieveRISConnections = async (
@@ -240,7 +144,7 @@ class TimetableService {
 				.filter((journeyRaw: any) => journeyRaw?.train?.journeyId)
 				.map(async (journeyRaw: any) => ({
 					entries: [
-						await this.mapToJourney(journeyRaw, {
+						await this.helper.mapToJourney(journeyRaw, {
 							isBahnhofProfile: false,
 							isDeparture: type === RequestType.DEPARTURES
 						})
@@ -273,7 +177,7 @@ class TimetableService {
 						journeyRaw
 							.filter((connectionRaw) => connectionRaw?.journeyID)
 							.map((connectionRaw) =>
-								this.mapToJourney(connectionRaw, {
+								this.helper.mapToJourney(connectionRaw, {
 									isBahnhofProfile: true,
 									isDeparture: type === RequestType.DEPARTURES
 								})
@@ -384,7 +288,7 @@ class TimetableService {
 		return await Promise.all(
 			values.map(async (entry) => ({
 				entries: [
-					await this.mapToJourney(entry, {
+					await this.helper.mapToJourney(entry, {
 						isBahnhofProfile: false,
 						isDeparture: type === RequestType.DEPARTURES
 					})
@@ -392,151 +296,6 @@ class TimetableService {
 			}))
 		);
 	};
-
-	private mapToJourney = async (
-		entry: any,
-		options: {
-			isBahnhofProfile: boolean;
-			isDeparture: boolean;
-		} = { isBahnhofProfile: false, isDeparture: true }
-	): Promise<typeof SingleTimetableEntrySchema.static> => {
-		const journeyId = entry?.journeyID ?? entry?.train?.journeyId ?? entry?.journeyId;
-		const isHAFAS = journeyId?.startsWith("2|#") ?? false;
-
-		const getOrigin = async (): Promise<TimetableStop> => {
-			if (isHAFAS) {
-				const result = (
-					await database
-						.select({ name: stations.name })
-						.from(stations)
-						.where(eq(stations.evaNumber, entry?.originEvaNumber))
-				)[0];
-
-				return {
-					name: result?.name ?? "NaN",
-					evaNumber: entry?.originEvaNumber,
-					cancelled: false
-				} as TimetableStop;
-			}
-			if (options.isBahnhofProfile)
-				return options.isDeparture ? this.mapToSmallStop(entry?.stopPlace) : this.mapToSmallStop(entry?.origin);
-			return options.isDeparture ? this.mapToSmallStop(entry?.station) : this.mapToSmallStop(entry?.origin);
-		};
-		const getDestination = async (): Promise<TimetableStop> => {
-			if (isHAFAS) {
-				const result = (
-					await database
-						.select({ name: stations.name })
-						.from(stations)
-						.where(eq(stations.evaNumber, entry?.destinationEvaNumber))
-				)[0];
-
-				return {
-					name: result?.name ?? "NaN",
-					evaNumber: entry?.destinationEvaNumber,
-					cancelled: false
-				} as TimetableStop;
-			}
-			if (options.isBahnhofProfile)
-				return options.isDeparture ? this.mapToSmallStop(entry?.destination) : this.mapToSmallStop(entry?.stopPlace);
-			return options.isDeparture ? this.mapToSmallStop(entry?.destination) : this.mapToSmallStop(entry?.station);
-		};
-		const isCancelled = () => {
-			if (isHAFAS) return (entry?.meldungen ?? []).some((message: any) => message.type === "HALT_AUSFALL");
-			return entry?.canceled ?? false;
-		};
-
-		const journey = {
-			ris_journeyId: !isHAFAS ? journeyId : undefined,
-			hafas_journeyId: isHAFAS ? journeyId : undefined,
-			origin: await getOrigin(),
-			destination: await getDestination(),
-			cancelled: isCancelled(),
-			timeInformation: this.mapToTime(entry),
-			lineInformation: {
-				productType: mapToProduct(entry?.type ?? entry?.train?.type ?? entry?.verkehrmittel?.produktGattung),
-				productName: options.isBahnhofProfile
-					? extractLeadingLetters(entry?.lineName)
-					: (entry?.train?.category ?? entry?.verkehrmittel?.kurzText),
-				journeyNumber: options.isBahnhofProfile
-					? undefined
-					: (entry?.train?.no ?? extractJourneyNumber(entry?.verkehrmittel?.name)),
-				journeyName:
-					entry?.lineName ??
-					entry?.verkehrmittel?.mittelText ??
-					entry?.train?.category + " " + entry?.train?.lineName,
-				additionalJourneyName: !options.isBahnhofProfile ? undefined : entry?.additionalLineName,
-				operator: options.isBahnhofProfile
-					? undefined
-					: {
-							code: entry?.administration?.id,
-							name: entry?.administration?.operatorName
-						}
-			},
-			viaStops: (entry?.viaStops ?? entry?.ueber ?? []).map((rawStop: any) => this.mapToSmallStop(rawStop)),
-			messages: !options.isBahnhofProfile
-				? []
-				: entry?.messages?.common
-						.concat(entry?.messages?.delay)
-						.concat(entry?.messages?.cancelation)
-						.concat(entry?.messages?.destination)
-						.concat(entry?.messages?.via)
-						.map((messageRaw: any) => this.mapMessage(messageRaw))
-		} as typeof SingleTimetableEntrySchema.static;
-
-		if (isHAFAS) journey.messages = (entry?.meldungen ?? []).map((message: any) => this.mapMessage(message, true));
-		if (journey.viaStops?.length === 0) journey.viaStops = undefined;
-		return journey;
-	};
-
-	private mapToSmallStop = (stop: any): TimetableStop => ({
-		name: stop?.name,
-		evaNumber: Number(stop?.evaNumber ?? stop?.evaNo),
-		cancelled: stop?.canceled ?? false,
-		additional: stop?.additional,
-		separation: stop?.separation,
-		nameParts:
-			stop?.nameParts?.map((rawPart: any) => ({
-				type: rawPart.type,
-				value: rawPart.value
-			})) ?? undefined
-	});
-
-	private mapToTime = (entry: any): Time => {
-		const plannedTime = DateTime.fromISO(entry?.time ?? entry?.timeSchedule ?? entry?.zeit);
-		let actualTime = DateTime.fromISO(entry?.timePredicted ?? entry?.timeDelayed ?? entry?.ezZeit);
-		if (!actualTime.isValid) actualTime = plannedTime;
-
-		const delay: number = actualTime.diff(plannedTime, ["seconds"]).seconds;
-
-		const plannedPlatform = entry?.platform ?? entry?.gleis;
-		let actualPlatform = entry?.platformPredicted ?? entry?.platformSchedule ?? entry?.ezGleis;
-		if (!actualPlatform) actualPlatform = plannedPlatform;
-
-		return {
-			plannedTime: plannedTime.toISO(),
-			actualTime: actualTime.toISO(),
-			delay: delay,
-			plannedPlatform: plannedPlatform,
-			actualPlatform: actualPlatform
-		} as Time;
-	};
-
-	private mapMessage = (entry: any, isHAFAS: boolean = false): Message => {
-		if (isHAFAS) {
-			const type = entry?.prioritaet === "HOCH" && entry?.type === "HALT_AUSFALL" ? "canceled-trip" : "general-warning";
-			return {
-				type: type,
-				text: entry?.text
-			} as Message;
-		}
-
-		return {
-			type: entry?.type,
-			text: entry?.text,
-			links: entry?.links ?? undefined
-		};
-	};
 }
 
-export { TimetableService, RequestType, Profile };
+export { TimetableService, Profile, RequestType };
