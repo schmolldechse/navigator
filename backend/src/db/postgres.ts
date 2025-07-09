b3a7cd6a2dd87b48217ba39c777040fbdab02049
Christian Knapp <christian.alex.knapp@icloud.com>
2025-07-09 16:26:04 +0200

    fix(backend): ensure reconnection of the database

--- backend/src/db/postgres.ts
+++ backend/src/db/postgres.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
-const pool = new Pool({ connectionString: Bun.env.POSTGRES_CONNECTION_STRING });
+const pool = new Pool({ connectionString: Bun.env.POSTGRES_CONNECTION_STRING, keepAlive: true });
 
 const database = drizzle(pool);
 
