2f65bc5f439f3206baf6b50e142a2888e4073a3c
Christian Knapp <christian.alex.knapp@icloud.com>
2025-09-26 09:36:25 +0200

    refactor: updated sql query

--- scripts/query_delay_stats.sql
+++ scripts/query_delay_stats.sql
@@ -1,102 +1,105 @@
 WITH date_range AS (
     SELECT generate_series(:start_date::date, :end_date::date, '1 day'::interval)::date AS date
 ),
-allowed_products AS (
-    SELECT DISTINCT name AS product_type
-    FROM core.station_products
+included_station_transports AS (
+    SELECT DISTINCT transport_name
+    FROM core.station_transports
     WHERE (
-        :evaNumbers::integer[] IS NULL
-        OR COALESCE(array_length(:evaNumbers::integer[], 1), 0) = 0
-        OR eva_number = ANY (:evaNumbers::integer[])
+        :includedEvaNumbers::integer[] IS NULL
+        OR COALESCE(array_length(:includedEvaNumbers::integer[], 1), 0) = 0
+        OR eva_number = ANY (:includedEvaNumbers::integer[])
     )
     AND (
-        NOT :onlyActiveProducts::bool
+        NOT :onlyIncludeActiveTransports::bool
         OR querying_enabled IS TRUE
     )
     AND (
-        :productTypes::varchar[] IS NULL
-        OR COALESCE(array_length(:productTypes::varchar[], 1), 0) = 0
-        OR name ILIKE ANY (:productTypes::varchar[])
+        :includedTransportTypes::varchar[] IS NULL
+        OR COALESCE(array_length(:includedTransportTypes::varchar[], 1), 0) = 0
+        OR transport_name ILIKE ANY (:includedTransportTypes::varchar[])
     )
 ),
-date_product_combinations AS (
-    SELECT date_range.date, allowed_products.product_type
+date_transport_combinstion AS (
+    SELECT date_range.date, included_station_transports.transport_name
     FROM date_range
-    CROSS JOIN allowed_products
+    CROSS JOIN included_station_transports
 )
 SELECT
     combination.date AS date,
-    journeys.product_type AS product_type,
-    --- Arrival Count
-    COALESCE(SUM(1) FILTER (WHERE via_stops.arrival_planned_time IS NOT NULL AND via_stops.arrival_actual_time IS NOT NULL), 0) AS arrival_count,
-    --- Arrival Delay
+    journey_transports.type AS transport_type,
+    --- Arrival Specific Details
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'ARRIVAL'), 0) AS arrival_count,
     COALESCE(SUM (CASE
-        WHEN :punishCancelled::bool AND via_stops.cancelled::bool
+        WHEN :punishCancellations::bool AND via_stop.cancelled::bool
+             THEN :delayThreshold::integer
+        ELSE via_stop.delay
+        END) FILTER (WHERE via_stop.type = 'ARRIVAL'), 0) AS arrival_delay_sum,
+    COALESCE(AVG (CASE
+        WHEN :punishCancellations::bool AND via_stop.cancelled::bool
             THEN :delayThreshold::integer
-        ELSE via_stops.arrival_delay
-        END) FILTER (WHERE via_stops.arrival_planned_time IS NOT NULL AND via_stops.arrival_actual_time IS NOT NULL), 0) AS arrival_delay_sum,
-    COALESCE(AVG(CASE
-        WHEN :punishCancelled::bool AND via_stops.cancelled::bool
+        ELSE via_stop.delay
+        END) FILTER (WHERE via_stop.type = 'ARRIVAL'), 0) AS arrival_delay_avg,
+    COALESCE(MIN(via_stop.delay) FILTER (WHERE via_stop.type = 'ARRIVAL'), 0) AS arrival_delay_min,
+    COALESCE(MAX(CASE
+        WHEN :punishCancellations::bool AND via_stop.cancelled::bool
             THEN :delayThreshold::integer
-        ELSE via_stops.arrival_delay
-        END) FILTER (WHERE via_stops.arrival_planned_time IS NOT NULL AND via_stops.arrival_actual_time IS NOT NULL), 0) AS arrival_delay_avg,
-    COALESCE(MIN (via_stops.arrival_delay) FILTER (WHERE via_stops.arrival_planned_time IS NOT NULL AND via_stops.arrival_actual_time IS NOT NULL), 0) AS arrival_delay_min,
-    COALESCE(MAX (CASE
-        WHEN :punishCancelled::bool AND via_stops.cancelled::bool
-            THEN :delayThreshold::integer
-        ELSE via_stops.arrival_delay
-        END) FILTER (WHERE via_stops.arrival_planned_time IS NOT NULL AND via_stops.arrival_actual_time IS NOT NULL), 0) AS arrival_delay_max,
-    --- Arrival Platform Changes
-    COALESCE(SUM (1) FILTER (WHERE via_stops.arrival_actual_platform <> via_stops.arrival_planned_platform), 0) AS arrival_platform_changes,
-    --- Arrival Too Early, Punctual, Too Delayed Counts
-    COALESCE(SUM (1) FILTER (WHERE via_stops.cancelled::bool IS FALSE AND via_stops.arrival_delay < 0), 0) AS arrival_too_early_count,
-    COALESCE(SUM (1) FILTER (WHERE via_stops.cancelled::bool IS FALSE AND via_stops.arrival_delay >= 0 AND via_stops.arrival_delay <= :delayThreshold::integer), 0) AS arrival_punctual_count,
-    COALESCE(SUM (1) FILTER (WHERE via_stops.arrival_delay > :delayThreshold::integer OR (:punishCancelled::bool AND via_stops.cancelled::bool AND via_stops.arrival_planned_time IS NOT NULL AND arrival_actual_time IS NOT NULL)), 0) AS arrival_too_delayed_count,
-    --- Departure Count
-    COALESCE(SUM(1) FILTER (WHERE via_stops.departure_planned_time IS NOT NULL AND via_stops.departure_actual_time IS NOT NULL), 0) AS departure_count,
-    --- Departure Delay
+        ELSE via_stop.delay
+        END) FILTER (WHERE via_stop.type = 'ARRIVAL'), 0) AS arrival_delay_max,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.cancelled::bool IS FALSE AND via_stop.type = 'ARRIVAL' AND via_stop.delay < 0), 0) AS arrival_too_early_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.cancelled::bool IS FALSE AND via_stop.type = 'ARRIVAL' AND via_stop.delay >= 0 AND via_stop.delay <= :delayThreshold::integer), 0) AS arrival_punctual_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'ARRIVAL' AND (via_stop.delay > :delayThreshold::integer OR (via_stop.cancelled::bool AND :punishCancellations::bool))), 0) AS arrival_delayed_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'ARRIVAL' AND via_stop.planned_platform <> via_stop.actual_platform), 0) AS arrival_platform_changes,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'ARRIVAL' AND via_stop.cancelled), 0) AS arrival_cancellation_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'ARRIVAL' AND via_stop.additional), 0) AS arrival_additional_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'ARRIVAL' AND via_stop.demand), 0) AS arrival_demand_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'ARRIVAL' AND via_stop.no_passenger_change), 0) AS arrival_no_exchange_possible_count,
+    --- Departure Specific Details
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'DEPARTURE'), 0) AS departure_count,
     COALESCE(SUM (CASE
-        WHEN :punishCancelled::bool AND via_stops.cancelled::bool
+        WHEN :punishCancellations::bool AND via_stop.cancelled::bool
             THEN :delayThreshold::integer
-        ELSE via_stops.departure_delay
-        END) FILTER (WHERE via_stops.departure_planned_time IS NOT NULL AND via_stops.departure_actual_time IS NOT NULL), 0) AS departure_delay_sum,
-    COALESCE(AVG(CASE
-        WHEN :punishCancelled::bool AND via_stops.cancelled::bool
+        ELSE via_stop.delay
+        END) FILTER (WHERE via_stop.type = 'DEPARTURE'), 0) AS departure_delay_sum,
+    COALESCE(AVG (CASE
+        WHEN :punishCancellations::bool AND via_stop.cancelled::bool
             THEN :delayThreshold::integer
-        ELSE via_stops.departure_delay
-        END) FILTER (WHERE via_stops.departure_planned_time IS NOT NULL AND via_stops.departure_actual_time IS NOT NULL), 0) AS departure_delay_avg,
-    COALESCE(MIN (via_stops.departure_delay) FILTER (WHERE via_stops.departure_planned_time IS NOT NULL AND via_stops.departure_actual_time IS NOT NULL), 0) AS departure_delay_min,
-    COALESCE(MAX (CASE
-        WHEN :punishCancelled::bool AND via_stops.cancelled::bool
+        ELSE via_stop.delay
+        END) FILTER (WHERE via_stop.type = 'DEPARTURE'), 0) AS departure_delay_avg,
+    COALESCE(MIN(via_stop.delay) FILTER (WHERE via_stop.type = 'DEPARTURE'), 0) AS departure_delay_min,
+    COALESCE(MAX(CASE
+        WHEN :punishCancellations::bool AND via_stop.cancelled::bool
             THEN :delayThreshold::integer
-        ELSE via_stops.departure_delay
-        END) FILTER (WHERE via_stops.departure_planned_time IS NOT NULL AND via_stops.departure_actual_time IS NOT NULL), 0) AS departure_delay_max,
-    --- Departure Platform Changes
-    COALESCE(SUM (1) FILTER (WHERE via_stops.departure_actual_platform <> via_stops.departure_planned_platform), 0) AS departure_platform_changes,
-    --- Departure Too Early, Punctual, Too Delayed Counts
-    COALESCE(SUM (1) FILTER (WHERE via_stops.cancelled::bool IS FALSE AND via_stops.departure_delay < 0), 0) AS departure_too_early_count,
-    COALESCE(SUM (1) FILTER (WHERE via_stops.cancelled::bool IS FALSE AND via_stops.departure_delay >= 0 AND via_stops.departure_delay <= :delayThreshold::integer), 0) AS departure_punctual_count,
-    COALESCE(SUM (1) FILTER (WHERE via_stops.departure_delay > :delayThreshold::integer OR (:punishCancelled::bool AND via_stops.cancelled::bool AND via_stops.departure_planned_time IS NOT NULL AND departure_actual_time IS NOT NULL)), 0) AS departure_too_delayed_count,
-    -- Cancellations
-    COALESCE(SUM(1) FILTER (WHERE via_stops.cancelled::bool), 0) AS cancellations
-FROM date_product_combinations combination
-LEFT JOIN core.journeys ON combination.date = journeys.journey_date AND combination.product_type ILIKE journeys.product_type
+        ELSE via_stop.delay
+        END) FILTER (WHERE via_stop.type = 'DEPARTURE'), 0) AS departure_delay_max,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.cancelled::bool IS FALSE AND via_stop.type = 'DEPARTURE' AND via_stop.delay < 0), 0) AS departure_too_early_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.cancelled::bool IS FALSE AND via_stop.type = 'DEPARTURE' AND via_stop.delay >= 0 AND via_stop.delay <= :delayThreshold::integer), 0) AS departure_punctual_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'DEPARTURE' AND (via_stop.delay > :delayThreshold::integer OR (via_stop.cancelled::bool AND :punishCancellations::bool))), 0) AS departure_delayed_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'DEPARTURE' AND via_stop.planned_platform <> via_stop.actual_platform), 0) AS departure_platform_changes,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'DEPARTURE' AND via_stop.cancelled), 0) AS departure_cancellation_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'DEPARTURE' AND via_stop.additional), 0) AS departure_additional_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'DEPARTURE' AND via_stop.demand), 0) AS departure_demand_count,
+    COALESCE(SUM(1) FILTER (WHERE via_stop.type = 'DEPARTURE' AND via_stop.no_passenger_change), 0) AS departure_no_exchange_possible_count
+FROM date_transport_combinstion combination
+LEFT JOIN core.journeys ON combination.date = journeys.date
+LEFT JOIN core.journey_transports ON journeys.journey_id = journey_transports.journey_id AND combination.transport_name = journey_transports.type
     AND (
-        :journeyName::varchar IS NULL
-        OR :journeyName::varchar = ''
-        OR journeys.journey_name ~ :journeyName::varchar
+        :includeJourneysByName::varchar IS NULL
+        OR :includeJourneysByName::varchar = ''
+        OR journey_transports.category ~ :includeJourneysByName::varchar
+        OR journey_transports.journey_description ~ :includeJourneysByName::varchar
+        OR (journey_transports.category || ' ' || journey_transports.line) ~ :includeJourneysByName::varchar
     )
     AND (
-        :journeyNumber::varchar IS NULL
-        OR :journeyNumber::varchar = ''
-        OR journeys.journey_number ~ :journeyNumber::varchar
+        :includeJourneysByNumber::integer[] IS NULL
+        OR COALESCE(array_length(:includeJourneysByNumber::integer[], 1), 0) = 0
+        OR journey_transports.number = ANY (:includeJourneysByNumber::integer[])
     )
-LEFT JOIN core."journey_via-stops" AS via_stops ON journeys.journey_id = via_stops.journey_id AND journeys.journey_date = via_stops.journey_date
+LEFT JOIN core.journey_scheduled_stop_places AS via_stop ON journeys.journey_id = via_stop.journey_id
     AND (
-        :evaNumbers::integer[] IS NULL
-        OR COALESCE(array_length(:evaNumbers::integer[], 1), 0) = 0
-        OR via_stops.eva_number = ANY (:evaNumbers::integer[])
+        :includedEvaNumbers::integer[] IS NULL
+        OR COALESCE(array_length(:includedEvaNumbers::integer[], 1), 0) = 0
+        OR via_stop.station_eva_number = ANY (:includedEvaNumbers::integer[])
     )
-WHERE journeys.product_type IS NOT NULL AND journeys.product_type <> ''
-GROUP BY combination.date, journeys.product_type
+WHERE journey_transports.type IS NOT NULL AND journey_transports.type <> ''
+GROUP BY combination.date, journey_transports.type
 ORDER BY combination.date;
