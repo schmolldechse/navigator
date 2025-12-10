1ad683036b327a3730e5704766f8d9b58e7515f8
Christian Knapp <christian.alex.knapp@icloud.com>
2025-12-10 22:21:15 +0100

    fix: received BadRequest as no JsonPropertyName were available

--- backend/Navigator.Data/Models/Station/VendoStationsBySearchRequest.cs
+++ backend/Navigator.Data/Models/Station/VendoStationsBySearchRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace Navigator.Data.Models.Station;
+﻿using System.Text.Json.Serialization;
+
+namespace Navigator.Data.Models.Station;
 
 public class VendoStationsBySearchRequest
 {
+    [JsonPropertyName("searchTerm")]
     public required string SearchTerm { get; set; }
+    [JsonPropertyName("maxResults")]
     public int? MaxResults { get; set; } = 10;
+    [JsonPropertyName("locationTypes")]
     public string[]? LocationTypes { get; set; } = ["ALL"];
 }
