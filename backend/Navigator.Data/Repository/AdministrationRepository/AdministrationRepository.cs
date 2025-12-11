345acd2d9ead0dac873c322afb155d238d7df977
Christian Knapp <christian.alex.knapp@icloud.com>
2025-12-11 22:40:40 +0100

    feat: add administration repository

---
+++ backend/Navigator.Data/Repository/AdministrationRepository/AdministrationRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Navigator.Data.Entities.Journey;
+
+namespace Navigator.Data.Repository.AdministrationRepository;
+
+public class AdministrationRepository(DataContext dataContext) : IAdministrationRepository
+{
+    public async Task<Administration?> GetAdministrationAsync(string administrationId, string operatorCode, string operatorName) => await dataContext.Administrations
+        .FirstOrDefaultAsync(administration => administration.AdministrationId == administrationId
+            && administration.OperatorCode == operatorCode
+            && administration.OperatorName == operatorName);
+
+    public async Task SaveAdministrationAsync(Administration administration)
+    {
+        if (await GetAdministrationAsync(administration.AdministrationId, administration.OperatorCode, administration.OperatorName) != null) return;
+        await dataContext.Administrations.AddAsync(administration);
+        await dataContext.SaveChangesAsync();
+    }
+}
