f7819cc2b2956abb7d17a47c69b243bb9395605d
Christian Knapp <christian.alex.knapp@icloud.com>
2025-12-15 19:11:10 +0100

    fix: use ISO 8601 time with timezone offset

--- backend/Navigator.Data/Models/Timetable/RisBoardRequest.cs
+++ backend/Navigator.Data/Models/Timetable/RisBoardRequest.cs
@@ -5,7 +5,7 @@ namespace Navigator.Data.Models.Timetable;
 public class RisBoardRequest
 {
     public required int EvaNumber { get; set; }
-    public required DateTime TimeStart { get; set; } = DateTime.Now;
+    public required DateTimeOffset TimeStart { get; set; } = DateTimeOffset.Now;
 
     [Range(0, 720)]
     public int Duration { get; set; } = 60;
